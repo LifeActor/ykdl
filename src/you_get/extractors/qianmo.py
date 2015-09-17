954d7283817f04f5cd3ac466ae7c15ceb1e502c3
Zhang Ning <zhangn1985@gmail.com>
2015-09-17 22:14:49 +0800

    rewrite qianmo using VideoExtractor
    for #1
    
    Change-Id: Ie8926329b723331b2cb6acf0ffa2fc3939895c2b
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/extractors/qianmo.py
+++ src/you_get/extractors/qianmo.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python
 
-__all__ = ['qianmo_download']
-
 from ..common import *
-import urllib.error
+from ..extractor import VideoExtractor
 import json
 
-def qianmo_download(url, output_dir = '.', merge = False, info_only = False):
-    if re.match(r'http://qianmo.com/\w+', url):
-        html = get_html(url)
-        match = re.search(r'(.+?)var video =(.+?);', html)
-        
-        if match:
-            video_info_json = json.loads(match.group(2))
-            title = video_info_json['title']
-            ext_video_id = video_info_json['ext_video_id']
-        
-        html = get_content('http://v.qianmo.com/player/{ext_video_id}'.format(ext_video_id = ext_video_id))
+class Qianmo(VideoExtractor):
+    name = "阡陌 (qianmo)"
+
+    stream_types = [
+        {'id': 'hd', 'container': 'mp4', 'video_profile': 'hd'},
+    ]
+
+    def prepare(self, **kwargs):
+        assert self.url or self.vid
+
+        if self.url and not self.vid:
+            html = get_html(self.url)
+            match = re.search(r'(.+?)var video =(.+?);', html)
+            if match:
+                video_info_json = json.loads(match.group(2))
+                self.title = video_info_json['title']
+                self.vid = video_info_json['ext_video_id']
+            else:
+                raise ValueError
+        else:
+            self.title = title or self.vid
+
+        html = get_content('http://v.qianmo.com/player/{}'.format(self.vid))
         c = json.loads(html)
-        url_list = []
-        for i in c['seg']:  #Cannot do list comprehensions
-            for a in c['seg'][i]:
-                for b in a['url']:
-                    url_list.append(b[0])
-        
-        type_ = ''
-        size = 0
-        for url in url_list:
-            _, type_, temp = url_info(url)
-            size += temp
+        for stream in self.stream_types:
+            if stream['id'] in c['seg'].keys():
+                urls = []
+                size = 0
+                for info in c['seg'][stream['id']][0]['url']:
+                    size += url_size(info[0])
+                    urls.append(info[0])
+                self.streams[stream['id']] = {'container': 'mp4','video_profile': stream['video_profile'], 'src': urls, 'size' : size}
 
-        type, ext, size = url_info(url)
-        print_info(site_info, title, type_, size)
-        if not info_only:
-            download_urls(url_list, title, type_, total_size=None, output_dir=output_dir, merge=merge)
-
-site_info = "qianmo"
-download = qianmo_download
+site = Qianmo()
+download = site.download_by_url
 download_playlist = playlist_not_supported('qianmo')

e1c08f30cefc56ec4c73a80d5a171b4ea110cc1c
Zhang Ning <zhangn1985@gmail.com>
2015-09-20 16:50:34 +0800

    remove merge for sina
    Change-Id: I5052a63b1dba9fed8912dafd54d71ef91405c93e
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/extractors/sina.py
+++ src/you_get/extractors/sina.py
@@ -24,16 +24,16 @@ def video_info(xml):
     vstr = match1(xml, r'<vstr>(?:<!\[CDATA\[)?(.+?)(?:\]\]>)?</vstr>')
     return urls, name, vstr
 
-def sina_download_by_vid(vid, title=None, output_dir='.', merge=True, info_only=False):
+def sina_download_by_vid(vid, title=None, output_dir='.',info_only=False):
     """Downloads a Sina video by its unique vid.
     http://video.sina.com.cn/
     """
 
     xml = video_info_xml(vid)
-    sina_download_by_xml(xml, title, output_dir, merge, info_only)
+    sina_download_by_xml(xml, title, output_dir, info_only)
 
 
-def sina_download_by_xml(xml, title, output_dir, merge, info_only):
+def sina_download_by_xml(xml, title, output_dir, info_only):
     urls, name, vstr = video_info(xml)
     title = title or name
     assert title
@@ -44,9 +44,9 @@ def sina_download_by_xml(xml, title, output_dir, merge, info_only):
 
     print_info(site_info, title, 'flv', size)
     if not info_only:
-        download_urls(urls, title, 'flv', size, output_dir = output_dir, merge = merge)
+        download_urls(urls, title, 'flv', size, output_dir = output_dir)
 
-def sina_download_by_vkey(vkey, title=None, output_dir='.', merge=True, info_only=False):
+def sina_download_by_vkey(vkey, title=None, output_dir='.',info_only=False):
     """Downloads a Sina video by its unique vkey.
     http://video.sina.com/
     """
@@ -56,9 +56,9 @@ def sina_download_by_vkey(vkey, title=None, output_dir='.', merge=True, info_onl
 
     print_info(site_info, title, 'flv', size)
     if not info_only:
-        download_urls([url], title, 'flv', size, output_dir = output_dir, merge = merge)
+        download_urls([url], title, 'flv', size, output_dir = output_dir)
 
-def sina_download(url, output_dir='.', merge=True, info_only=False):
+def sina_download(url, output_dir='.', info_only=False):
     """Downloads Sina videos by URL.
     """
 
@@ -72,11 +72,11 @@ def sina_download(url, output_dir='.', merge=True, info_only=False):
 
     if vid:
         title = match1(video_page, r'title\s*:\s*\'([^\']+)\'')
-        sina_download_by_vid(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
+        sina_download_by_vid(vid, title=title, output_dir=output_dir,  info_only=info_only)
     else:
         vkey = match1(video_page, r'vkey\s*:\s*"([^"]+)"')
         title = match1(video_page, r'title\s*:\s*"([^"]+)"')
-        sina_download_by_vkey(vkey, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
+        sina_download_by_vkey(vkey, title=title, output_dir=output_dir, info_only=info_only)
 
 site_info = "Sina.com"
 download = sina_download

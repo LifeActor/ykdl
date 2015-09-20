279fd09d7b11a134966e645e5419d513764b2490
Zhang Ning <zhangn1985@gmail.com>
2015-09-20 15:11:16 +0800

    player cache option support
    mpv and mplayer
    
    for #5
    
    Change-Id: I75f0e6ff8373c6f5efd2cf238c307ca39d87ba4a
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/common.py
+++ src/you_get/common.py
@@ -100,11 +100,18 @@ def matchall(text, patterns):
 def launch_player(player, urls, size):
     import subprocess
     import shlex
-    if size > 2.14748e+09:
-        size = 2.14748e+09
-
     if size:
-        player += " --cache={}".format(int(size/1024))
+        size_kb = int(size/1024) + 1
+        if 'mpv' in player:
+            if size_kb > 2147483647:
+                size_kb = 2147483647
+            player += " --cache={}".format(size_kb)
+        elif 'mplayer' in player:
+            player += " --cache={} --cache-min=1".format(size_kb)
+        else:
+            log.d("your player: {} 's cache option is not supported!!".format(player))
+            log.d("please report issue to https://github.com/zhangn1985/you-get/issues/5")
+            log.d("thanks a lot!")
     subprocess.call(shlex.split(player) + list(urls))
 
 def parse_query_param(url, param):

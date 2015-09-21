f356f146bd0859e1cba5e7b20130cbdc0f1043b4
Zhang Ning <zhangn1985@gmail.com>
2015-09-21 22:10:57 +0800

    rewrite bilibili
    bilibili is complicate site.
    it has original videos, and embed videos
    
    I must write a wrapper for it
    
    current embed video support levt, hunantv, youku
    
    wait for more sites
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/extractors/bilibili.py
+++ src/you_get/extractors/bilibili.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
-__all__ = ['bilibili_download']
-
 from ..common import *
-
-from .sina import sina_download_by_vid
-from .tudou import tudou_download_by_id
-from .youku import youku_download_by_vid
+from ..extractor import VideoExtractor
+from ..embedextractor import EmbedExtractor
 
 import hashlib
 import re
@@ -14,51 +10,6 @@ import re
 # API key provided by cnbeining
 appkey='85eb6835b0a1034e';
 secretkey = '2ad42749773c441109bdc0191257a664'
-client = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset': 'UTF-8,*;q=0.5',
-    'Accept-Encoding': 'gzip,deflate,sdch',
-    'Accept-Language': 'en-US,en;q=0.8',
-    #'User-Agent': 'Biligrab /0.8 (cnbeining@gmail.com)'
-    'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.94 Safari/537.36"
-}
-
-def get_srt_xml(id):
-    url = 'http://comment.bilibili.com/%s.xml' % id
-    return get_html(url)
-
-def parse_srt_p(p):
-    fields = p.split(',')
-    assert len(fields) == 8, fields
-    time, mode, font_size, font_color, pub_time, pool, user_id, history = fields
-    time = float(time)
-
-    mode = int(mode)
-    assert 1 <= mode <= 8
-    # mode 1~3: scrolling
-    # mode 4: bottom
-    # mode 5: top
-    # mode 6: reverse?
-    # mode 7: position
-    # mode 8: advanced
-
-    pool = int(pool)
-    assert 0 <= pool <= 2
-    # pool 0: normal
-    # pool 1: srt
-    # pool 2: special?
-
-    font_size = int(font_size)
-
-    font_color = '#%06x' % int(font_color)
-
-    return pool, mode, font_size, font_color
-
-def parse_srt_xml(xml):
-    d = re.findall(r'<d p="([^"]+)">(.*)</d>', xml)
-    for x, y in d:
-        p = parse_srt_p(x)
-    raise NotImplementedError()
 
 def parse_cid_playurl(xml):
     from xml.dom.minidom import parseString
@@ -69,87 +20,92 @@ def parse_cid_playurl(xml):
     except:
         return []
 
-def bilibili_download_by_cids(cids, title, output_dir='.',  info_only=False):
-    urls = []
-    for cid in cids:
-        sign_this = hashlib.md5(bytes('appkey=' + appkey + '&cid=' + cid + secretkey, 'utf-8')).hexdigest()
-        url = 'http://interface.bilibili.com/playurl?appkey=' + appkey + '&cid=' + cid + '&sign=' + sign_this
-        urls += [i
-                if not re.match(r'.*\.qqvideo\.tc\.qq\.com', i)
-                else re.sub(r'.*\.qqvideo\.tc\.qq\.com', 'http://vsrc.store.qq.com', i)
-                for i in parse_cid_playurl(get_content(url, headers=client))]
+class BiliOrig(VideoExtractor):
+    name = '哔哩哔哩 (Bilibili)'
 
-    type_ = ''
-    size = 0
-    for url in urls:
-        _, type_, temp = url_info(url)
-        size += temp
+    def prepare(self, **kwargs):
+        assert self.url or self.vid
 
-    print_info(site_info, title, type_, size)
-    if not info_only:
-        download_urls(urls, title, type_, total_size=None, output_dir=output_dir)
+        if not self.vid:
+            html = get_content(self.url)
+            self.vid = match1(html, '\"cid=([^&]+)')
+            self.title = match1(html, '<title>([^<]+)')
 
-def bilibili_download_by_cid(id, title, output_dir='.', info_only=False):
-    sign_this = hashlib.md5(bytes('appkey=' + appkey + '&cid=' + id + secretkey, 'utf-8')).hexdigest()
-    url = 'http://interface.bilibili.com/playurl?appkey=' + appkey + '&cid=' + id + '&sign=' + sign_this
-    urls = [i
-            if not re.match(r'.*\.qqvideo\.tc\.qq\.com', i)
-            else re.sub(r'.*\.qqvideo\.tc\.qq\.com', 'http://vsrc.store.qq.com', i)
-            for i in parse_cid_playurl(get_content(url, headers=client))]
-
-    type_ = ''
-    size = 0
-    for url in urls:
-        _, type_, temp = url_info(url)
-        size += temp or 0
-
-    print_info(site_info, title, type_, size)
-    if not info_only:
-        download_urls(urls, title, type_, total_size=None, output_dir=output_dir)
-
-def bilibili_download(url, output_dir='.', info_only=False):
-    html = get_html(url)
-
-    title = r1_of([r'<meta name="title" content="([^<>]{1,999})" />',r'<h1[^>]*>([^<>]+)</h1>'], html)
-    title = unescape_html(title)
-    title = escape_file_path(title)
-
-    flashvars = r1_of([r'(cid=\d+)', r'(cid: \d+)', r'flashvars="([^"]+)"', r'"https://[a-z]+\.bilibili\.com/secure,(cid=\d+)(?:&aid=\d+)?"'], html)
-    assert flashvars
-    flashvars = flashvars.replace(': ','=')
-    t, id = flashvars.split('=', 1)
-    id = id.split('&')[0]
-    if t == 'cid':
-        # Multi-P
-        cids = []
-        p = re.findall('<option value=\'([^\']*)\'>', html)
-        if not p:
-            bilibili_download_by_cid(id, title, output_dir=output_dir, info_only=info_only)
         else:
-            for i in p:
-                html = get_html("http://www.bilibili.com%s" % i)
-                flashvars = r1_of([r'(cid=\d+)', r'flashvars="([^"]+)"', r'"https://[a-z]+\.bilibili\.com/secure,(cid=\d+)(?:&aid=\d+)?"'], html)
-                if flashvars:
-                    t, cid = flashvars.split('=', 1)
-                    cids.append(cid.split('&')[0])
-            bilibili_download_by_cids(cids, title, output_dir=output_dir, info_only=info_only)
+            if 'title' in kwargs and kwargs['title']:
+                self.title = kwargs['title']
+            else:
+                self.title = self.name + "-" + self.vid
 
-    elif t == 'vid':
-        sina_download_by_vid(id, title, output_dir = output_dir, info_only = info_only)
-    elif t == 'ykid':
-        youku_download_by_vid(id, title=title, output_dir = output_dir, info_only = info_only)
-    elif t == 'uid':
-        tudou_download_by_id(id, title, output_dir = output_dir, info_only = info_only)
-    else:
-        raise NotImplementedError(flashvars)
+        assert self.vid
 
-    if not info_only:
-        title = get_filename(title)
-        print('Downloading %s ...\n' % (title + '.cmt.xml'))
-        xml = get_srt_xml(id)
-        with open(os.path.join(output_dir, title + '.cmt.xml'), 'w', encoding='utf-8') as x:
-            x.write(xml)
+        sign_this = hashlib.md5(bytes('appkey=' + appkey + '&cid=' + self.vid + secretkey, 'utf-8')).hexdigest()
+        api_url = 'http://interface.bilibili.com/playurl?appkey=' + appkey + '&cid=' + self.vid + '&sign=' + sign_this
+        urls = parse_cid_playurl(get_content(api_url))
 
-site_info = "bilibili.com"
-download = bilibili_download
-download_playlist = playlist_not_supported('bilibili')
+        ext = ''
+        size = 0
+        for url in urls:
+            _, ext, temp = url_info(url)
+            size += temp
+
+        self.stream_types.append('current')
+        self.streams['current'] = {'container': ext, 'video_profile': 'current', 'src' : urls, 'size': size}
+
+class BiliEmbed(EmbedExtractor):
+
+    def prepare(self, **kwargs):
+        assert self.url
+
+        html = get_content(self.url)
+        self.title = match1(html, '<title>([^<]+)')
+        vid = match1(html, '&vid=([^\"]+)')
+        if vid:
+            self.video_info.append(('letv', vid))
+
+        vid = match1(html, '&video_id=([^\"]+)')
+
+        if vid:
+            self.video_info.append(('hunantv', vid))
+
+        vid = match1(html, '&ykid=([^\"]+)')
+
+        if vid:
+            self.video_info.append(('youku', vid))
+
+class BiliBili():
+
+    def __init__(self, *args):
+        self.orig = BiliOrig()
+        self.embed = BiliEmbed()
+        if args:
+            self.url = args[0]
+
+    def download_by_url(self, url, **kwargs):
+        self.url = url
+
+        html = get_content(self.url)
+        self.title = match1(html, '<title>([^<]+)')
+        vids = matchall(html, ['\"cid=([^&]+)'])
+
+        if vids:
+            for vid in vids:
+                self.orig.download_by_vid(vid, title=self.title, **kwargs)
+        else:
+            self.embed.download(self.url, **kwargs)
+
+    def download_playlist_by_url(self, url, **kwargs):
+        self.url = url
+
+        html = get_content(self.url)
+
+        list = matchall(html, ['<option value=\'([^\']*)\'>'])
+
+        for l in list:
+            next_url = "http://www.bilibili.com{}".format(l)
+            self.download_by_url(next_url, **kwargs)
+
+
+site = BiliBili()
+download = site.download_by_url
+download_playlist = site.download_playlist_by_url

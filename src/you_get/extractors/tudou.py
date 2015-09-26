18ac704585451de5fb093f4585159fcdbfe9e4d7
Zhang Ning <zhangn1985@gmail.com>
2015-09-26 22:48:13 +0800

    rewrite Tudou, but Tudou Original video direct playback is broken
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/extractors/tudou.py
+++ src/you_get/extractors/tudou.py
@@ -1,84 +1,71 @@
 #!/usr/bin/env python
 
-__all__ = ['tudou_download', 'tudou_download_playlist', 'tudou_download_by_id', 'tudou_download_by_iid']
-
 from ..common import *
 from xml.dom.minidom import parseString
 
-def tudou_download_by_iid(iid, title, output_dir = '.', merge = True, info_only = False):
-    data = json.loads(get_decoded_html('http://www.tudou.com/outplay/goto/getItemSegs.action?iid=%s' % iid))
-    temp = max([data[i] for i in data if 'size' in data[i][0]], key=lambda x:sum([part['size'] for part in x]))
-    vids, size = [t["k"] for t in temp], sum([t["size"] for t in temp])
-    urls = [[n.firstChild.nodeValue.strip()
+from ..extractor import VideoExtractor
+
+class Tudou(VideoExtractor):
+    name = "土豆 (tudou)"
+
+    def prepare(self, **kwargs):
+        assert self.url or self.vid
+        html = ''
+        if self.url:
+            html = get_content(self.url)
+            self.title = match1(html, '<title>([^<]+)')
+            vcode = match1(html, 'vcode\s*[:=]\s*\'([^\']+)\'')
+            if vcode:
+                from .youku import site as youku
+                youku.download_by_vid(vcode, **kwargs)
+                exit(0)
+            else:
+                self.vid = match1(html, 'iid\s*[:=]\s*(\d+)')
+        else:
+            if 'title' in kwargs and kwargs['title']:
+                self.title = kwargs['title']
+            else:
+                self.title = self.name + self.vid
+
+        data = json.loads(get_decoded_html('http://www.tudou.com/outplay/goto/getItemSegs.action?iid=%s' % self.vid))
+
+        temp = max([data[i] for i in data if 'size' in data[i][0]], key=lambda x:sum([part['size'] for part in x]))
+        vids, size = [t["k"] for t in temp], sum([t["size"] for t in temp])
+        urls = [[n.firstChild.nodeValue.strip()
              for n in
                 parseString(
                     get_html('http://ct.v2.tudou.com/f?id=%s' % vid))
                 .getElementsByTagName('f')][0]
             for vid in vids]
+        ext = r1(r'http://[\w.]*/(\w+)/[\w.]*', urls[0])
+        self.stream_types.append('current')
+        self.streams['current'] = {'container': ext, 'video_profile': 'current', 'src' : urls, 'size': size}
 
-    ext = r1(r'http://[\w.]*/(\w+)/[\w.]*', urls[0])
-
-    print_info(site_info, title, ext, size)
-    if not info_only:
-        download_urls(urls, title, ext, size, output_dir=output_dir, merge = merge)
-
-def tudou_download_by_id(id, title, output_dir = '.', merge = True, info_only = False):
-    html = get_html('http://www.tudou.com/programs/view/%s/' % id)
-
-    iid = r1(r'iid\s*[:=]\s*(\S+)', html)
-    title = r1(r'kw\s*[:=]\s*[\'\"]([^\n]+?)\'\s*\n', html).replace("\\'", "\'")
-    tudou_download_by_iid(iid, title, output_dir = output_dir, merge = merge, info_only = info_only)
-
-def tudou_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    # Embedded player
-    id = r1(r'http://www.tudou.com/v/([^/]+)/', url)
-    if id:
-        return tudou_download_by_id(id, title="", info_only=info_only)
-
-    html = get_decoded_html(url)
-
-    title = r1(r'kw\s*[:=]\s*[\'\"]([^\n]+?)\'\s*\n', html).replace("\\'", "\'")
-    assert title
-    title = unescape_html(title)
-
-    vcode = r1(r'vcode\s*[:=]\s*\'([^\']+)\'', html)
-    if vcode:
-        from .youku import youku_download_by_vid
-        if 'stream_id' in kwargs:
-            return youku_download_by_vid(vcode, title=title, output_dir=output_dir, merge=merge, info_only=info_only, stream_id=kwargs['stream_id'])
+    def parse_playlist(self):
+        aid = r1('http://www.tudou.com/playlist/p/a(\d+)(?:i\d+)?\.html', url)
+        html = get_decoded_html(url)
+        if not aid:
+            aid = r1(r"aid\s*[:=]\s*'(\d+)'", html)
+        if re.match(r'http://www.tudou.com/albumcover/', url):
+            atitle = r1(r"title\s*:\s*'([^']+)'", html)
+        elif re.match(r'http://www.tudou.com/playlist/p/', url):
+            atitle = r1(r'atitle\s*=\s*"([^"]+)"', html)
         else:
-            return youku_download_by_vid(vcode, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
+            raise NotImplementedError(url)
+        assert aid
+        assert atitle
+        import json
+        #url = 'http://www.tudou.com/playlist/service/getZyAlbumItems.html?aid='+aid
+        url = 'http://www.tudou.com/playlist/service/getAlbumItems.html?aid='+aid
+        return [(atitle + '-' + x['title'], str(x['itemId'])) for x in json.loads(get_html(url))['message']]
 
-    iid = r1(r'iid\s*[:=]\s*(\d+)', html)
-    if not iid:
-        return tudou_download_playlist(url, output_dir, merge, info_only)
+    def download_playlist_by_url(self, url, **kwargs):
+        self.url = url
+        videos = parse_playlist()
+        for i, (title, id) in enumerate(videos):
+            print('Processing %s of %s videos...' % (i + 1, len(videos)))
+            self.download_by_vid(id, title=title, **kwargs)
 
-    tudou_download_by_iid(iid, title, output_dir = output_dir, merge = merge, info_only = info_only)
-
-def parse_playlist(url):
-    aid = r1('http://www.tudou.com/playlist/p/a(\d+)(?:i\d+)?\.html', url)
-    html = get_decoded_html(url)
-    if not aid:
-        aid = r1(r"aid\s*[:=]\s*'(\d+)'", html)
-    if re.match(r'http://www.tudou.com/albumcover/', url):
-        atitle = r1(r"title\s*:\s*'([^']+)'", html)
-    elif re.match(r'http://www.tudou.com/playlist/p/', url):
-        atitle = r1(r'atitle\s*=\s*"([^"]+)"', html)
-    else:
-        raise NotImplementedError(url)
-    assert aid
-    assert atitle
-    import json
-    #url = 'http://www.tudou.com/playlist/service/getZyAlbumItems.html?aid='+aid
-    url = 'http://www.tudou.com/playlist/service/getAlbumItems.html?aid='+aid
-    return [(atitle + '-' + x['title'], str(x['itemId'])) for x in json.loads(get_html(url))['message']]
-
-def tudou_download_playlist(url, output_dir = '.', merge = True, info_only = False):
-    videos = parse_playlist(url)
-    for i, (title, id) in enumerate(videos):
-        print('Processing %s of %s videos...' % (i + 1, len(videos)))
-        tudou_download_by_iid(id, title, output_dir = output_dir, merge = merge, info_only = info_only)
-
-site_info = "Tudou.com"
-download = tudou_download
-download_playlist = tudou_download_playlist
+site = Tudou()
+download = site.download_by_url
+download_playlist = site.download_playlist_by_url

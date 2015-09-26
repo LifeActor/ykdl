d98793ae0eb62e92b3d76d10fcdef444cb74c281
Zhang Ning <zhangn1985@gmail.com>
2015-09-26 17:40:50 +0800

    some change to get charset
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/common.py
+++ src/you_get/common.py
@@ -225,7 +225,7 @@ def get_content(url, headers=fake_headers, decoded=True):
     # Decode the response body
     if decoded:
         charset = match1(response.getheader('Content-Type'), r'charset=([\w-]+)') or \
-                  match1(str(data), r'charset=([\w-]+)')
+                  match1(str(data), r'charset=\"([^\"]+)')
         if charset is not None:
             data = data.decode(charset)
         else:

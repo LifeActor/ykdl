7b358b3b3eaee1a853d251d6d53dc516c3f8f801
Zhang Ning <zhangn1985@gmail.com>
2015-09-26 17:14:34 +0800

    rewrite miomio, broken by sina
    for #4
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/extractors/miomio.py
+++ src/you_get/extractors/miomio.py
@@ -1,32 +1,23 @@
 #!/usr/bin/env python
 
-__all__ = ['miomio_download']
-
 from ..common import *
+from ..embedextractor import EmbedExtractor
 
-from .sina import sina_download_by_xml
-from .tudou import tudou_download_by_id
-from .youku import youku_download_by_vid
+class Miomio(EmbedExtractor):
 
-def miomio_download(url, output_dir = '.', merge = True, info_only = False):
-    html = get_html(url)
+    def prepare(self, **kwargs):
+        assert self.url
+        html = get_content(self.url)
+        self.title = r1(r'<meta name="description" content="([^"]*)"', html)
+        flashvars = r1(r'flashvars="(type=[^"]*)"', html)
+        t = r1(r'type=(\w+)', flashvars)
+        id = r1(r'vid=([^&]+)', flashvars)
+        print(t,id)
+        if t == 'video':
+            t = 'sina'
 
-    title = r1(r'<meta name="description" content="([^"]*)"', html)
-    flashvars = r1(r'flashvars="(type=[^"]*)"', html)
+        self.video_info.append((t, id))
 
-    t = r1(r'type=(\w+)', flashvars)
-    id = r1(r'vid=([^"]+)', flashvars)
-    if t == 'youku':
-        youku_download_by_vid(id, title=title, output_dir=output_dir, info_only=info_only)
-    elif t == 'tudou':
-        tudou_download_by_id(id, title, output_dir=output_dir, info_only=info_only)
-    elif t == 'sina' or t=='video':
-        url = "http://www.miomio.tv/mioplayer/mioplayerconfigfiles/sina.php?vid=" + id
-        xml = get_content (url, headers=fake_headers, decoded=True)
-        sina_download_by_xml(xml, title, output_dir=output_dir, info_only=info_only)
-    else:
-        raise NotImplementedError(flashvars)
-
-site_info = "MioMio.tv"
-download = miomio_download
+site = Miomio()
+download = site.download
 download_playlist = playlist_not_supported('miomio')

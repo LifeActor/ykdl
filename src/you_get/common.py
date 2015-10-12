0e2fcc2acc06fb4672f4e49de1e62adb956d01ad
Zhang Ning <zhangn1985@gmail.com>
2015-10-12 22:18:21 +0800

    move html related to util
    for #7
    
    temp remove cookies
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/common.py
+++ src/you_get/common.py
@@ -15,6 +15,7 @@ from .util import log
 from .util.strings import get_filename, unescape_html
 from .util.progressbar import SimpleProgressBar, PiecesProgressBar
 from .util.match import match1, matchall, r1
+from .util.html import *
 
 dry_run = False
 force = False
@@ -22,18 +23,20 @@ player = None
 extractor_proxy = None
 cookies_txt = None
 
-fake_headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.2.1'
-}
+
 
 if sys.stdout.isatty():
     default_encoding = sys.stdout.encoding.lower()
 else:
     default_encoding = locale.getpreferredencoding().lower()
 
+# DEPRECATED in favor of util.legitimize()
+def escape_file_path(path):
+    path = path.replace('/', '-')
+    path = path.replace('\\', '-')
+    path = path.replace('*', '-')
+    path = path.replace('?', '-')
+    return path
 
 def launch_player(player, urls, size):
     import subprocess
@@ -54,192 +57,7 @@ def launch_player(player, urls, size):
             log.d("thanks a lot!")
     subprocess.call(shlex.split(player) + list(urls))
 
-def parse_query_param(url, param):
-    """Parses the query string of a URL and returns the value of a parameter.
 
-    Args:
-        url: A URL.
-        param: A string representing the name of the parameter.
-
-    Returns:
-        The value of the parameter.
-    """
-
-    try:
-        return parse.parse_qs(parse.urlparse(url).query)[param][0]
-    except:
-        return None
-
-def unicodize(text):
-    return re.sub(r'\\u([0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f])', lambda x: chr(int(x.group(0)[2:], 16)), text)
-
-# DEPRECATED in favor of util.legitimize()
-def escape_file_path(path):
-    path = path.replace('/', '-')
-    path = path.replace('\\', '-')
-    path = path.replace('*', '-')
-    path = path.replace('?', '-')
-    return path
-
-def ungzip(data):
-    """Decompresses data for Content-Encoding: gzip.
-    """
-    from io import BytesIO
-    import gzip
-    buffer = BytesIO(data)
-    f = gzip.GzipFile(fileobj=buffer)
-    return f.read()
-
-def undeflate(data):
-    """Decompresses data for Content-Encoding: deflate.
-    (the zlib compression is used.)
-    """
-    import zlib
-    decompressobj = zlib.decompressobj(-zlib.MAX_WBITS)
-    return decompressobj.decompress(data)+decompressobj.flush()
-
-# DEPRECATED in favor of get_content()
-def get_response(url, faker = False):
-    if faker:
-        response = request.urlopen(request.Request(url, headers = fake_headers), None)
-    else:
-        response = request.urlopen(url)
-
-    data = response.read()
-    if response.info().get('Content-Encoding') == 'gzip':
-        data = ungzip(data)
-    elif response.info().get('Content-Encoding') == 'deflate':
-        data = undeflate(data)
-    response.data = data
-    return response
-
-# DEPRECATED in favor of get_content()
-def get_html(url, encoding = None, faker = False):
-    content = get_response(url, faker).data
-    return str(content, 'utf-8', 'ignore')
-
-# DEPRECATED in favor of get_content()
-def get_decoded_html(url, faker = False):
-    response = get_response(url, faker)
-    data = response.data
-    charset = r1(r'charset=([\w-]+)', response.headers['content-type'])
-    if charset:
-        return data.decode(charset, 'ignore')
-    else:
-        return data
-
-def get_location(url):
-    response = request.urlopen(url)
-    # urllib will follow redirections and it's too much code to tell urllib
-    # not to do that
-    return response.geturl()
-
-def get_content(url, headers=fake_headers, decoded=True, data=None):
-    """Gets the content of a URL via sending a HTTP GET request.
-
-    Args:
-        url: A URL.
-        headers: Request headers used by the client.
-        decoded: Whether decode the response body using UTF-8 or the charset specified in Content-Type.
-
-    Returns:
-        The content as a string.
-    """
-
-    req = request.Request(url, headers=headers, data=data)
-    if cookies_txt:
-        cookies_txt.add_cookie_header(req)
-        req.headers.update(req.unredirected_hdrs)
-    response = request.urlopen(req)
-    data = response.read()
-
-    # Handle HTTP compression for gzip and deflate (zlib)
-    content_encoding = response.getheader('Content-Encoding')
-    if content_encoding == 'gzip':
-        data = ungzip(data)
-    elif content_encoding == 'deflate':
-        data = undeflate(data)
-
-    # Decode the response body
-    if decoded:
-        charset = match1(response.getheader('Content-Type'), r'charset=([\w-]+)') or \
-                  match1(str(data), r'charset=\"([^\"]+)')
-        if charset is not None:
-            data = data.decode(charset)
-        else:
-            data = data.decode('utf-8')
-
-    return data
-
-def url_size(url, faker = False):
-    if faker:
-        response = request.urlopen(request.Request(url, headers = fake_headers), None)
-    else:
-        response = request.urlopen(url)
-
-    size = response.headers['content-length']
-    return int(size) if size!=None else float('inf')
-
-# TO BE DEPRECATED
-# urls_size() does not have a faker
-# also it takes too long time
-def urls_size(urls):
-    return sum(map(url_size, urls))
-
-def url_info(url, faker = False):
-    if faker:
-        response = request.urlopen(request.Request(url, headers = fake_headers), None)
-    else:
-        response = request.urlopen(request.Request(url))
-
-    headers = response.headers
-
-    type = headers['content-type']
-    mapping = {
-        'video/3gpp': '3gp',
-        'video/f4v': 'flv',
-        'video/mp4': 'mp4',
-        'video/MP2T': 'ts',
-        'video/quicktime': 'mov',
-        'video/webm': 'webm',
-        'video/x-flv': 'flv',
-        'video/x-ms-asf': 'asf',
-        'audio/mp4': 'mp4',
-        'audio/mpeg': 'mp3'
-    }
-    if type in mapping:
-        ext = mapping[type]
-    else:
-        type = None
-        if headers['content-disposition']:
-            try:
-                filename = parse.unquote(r1(r'filename="?([^"]+)"?', headers['content-disposition']))
-                if len(filename.split('.')) > 1:
-                    ext = filename.split('.')[-1]
-                else:
-                    ext = None
-            except:
-                ext = None
-        else:
-            ext = None
-
-    if headers['transfer-encoding'] != 'chunked':
-        size = headers['content-length'] and int(headers['content-length'])
-    else:
-        size = None
-
-    return type, ext, size
-
-def url_locations(urls, faker = False):
-    locations = []
-    for url in urls:
-        if faker:
-            response = request.urlopen(request.Request(url, headers = fake_headers), None)
-        else:
-            response = request.urlopen(request.Request(url))
-
-        locations.append(response.url)
-    return locations
 
 def url_save(url, filepath, bar, refer = None, is_part = False, faker = False):
     file_size = url_size(url, faker = faker)

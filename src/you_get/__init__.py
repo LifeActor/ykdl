c8c6fd9253f17975ea051a0ad2773111b007a0ff
Zhang Ning <zhangn1985@gmail.com>
2015-10-24 17:41:03 +0800

    rewrite you-get start
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/__init__.py
+++ src/you_get/__init__.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 # This file is Python 2 compliant.
 
-import sys
+def main(**kwargs):
+    """Main entry point.
+    you-get (legacy)
+    """
+    from .common import main
+    main()
 
-if sys.version_info[0] == 3:
-    #from .extractor import Extractor, VideoExtractor
-    #from .util import log
-
-    from .__main__ import *
-
-    #from .common import *
-    #from .version import *
-    #from .cli_wrapper import *
-    #from .extractor import *
-else:
-    # Don't import anything.
-    pass
+if __name__ == '__main__':
+    main()

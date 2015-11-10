525c41eb8fd5d865658007019b7006d9e1616ff5
Zhang Ning <zhangn1985@gmail.com>
2015-11-10 20:38:25 +0800

    update acfun change the method to get video info
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- src/you_get/extractors/acfun.py
+++ src/you_get/extractors/acfun.py
@@ -1,40 +1,43 @@
 #!/usr/bin/env python
 
 from ..util.html import get_content
-from ..common import playlist_not_supported
 from ..util.match import match1, matchall
 from ..embedextractor import EmbedExtractor
-
-import re
-import json
 
 class Acfun(EmbedExtractor):
 
     def prepare(self, **kwargs):
         assert self.url
-        assert re.match(r'http://[^\.]+.acfun.[^\.]+/\D/\D\D(\d+)', self.url)
 
         html = get_content(self.url)
 
         self.title = match1(html, '<h1 id="txt-title-view">([^<>]+)<')
-        videos = matchall(html,["data-vid=\"(\d+)\""])
 
-        for video in videos:
-            info = json.loads(get_content('http://www.acfun.tv/video/getVideo.aspx?id=' + video))
-            sourceType = info['sourceType']
+        sourceType = match1(html, "data-from=\"([a-zA-Z0-9]+)\" data-did=")
+        sourceId = match1(html, "data-did=\"\" data-sid=\"([a-zA-Z0-9=]+)\"")
+        sourceVid = match1(html, "data-vid=\"([a-zA-Z0-9=]+)\" data-scode=")
 
-            if sourceType == 'letv':
-                #workaround for letv, because it is letvcloud
-                sourceType = 'letvcloud'
-                sourceId = (info['sourceId'], '2d8c027396')
-            if sourceType == 'zhuzhan':
-                sourceType = 'acorig'
-                sourceId = video
-            else:
-                sourceId = info['sourceId']
+        if sourceType == 'zhuzhan':
+            sourceType = 'acorig'
+            sourceId = sourceVid
+        elif sourceType == 'letv':
+            #workaround for letv, because it is letvcloud
+            sourceType = 'letvcloud'
+            sourceId = (sourceId, '2d8c027396')
 
-            self.video_info.append((sourceType, sourceId))
+        self.video_info.append((sourceType, sourceId))
+
+    def download_playlist_by_url(self, url, param, **kwargs):
+        self.url = url
+
+        html = get_content(self.url)
+
+        videos = matchall(html, ['href="([\/a-zA-Z0-9_]+)" title="Part'])
+        print(videos)
+        for v in videos:
+            next_url = "http://www.acfun.tv/{}".format(v)
+            self.download(next_url, param, **kwargs)
 
 site = Acfun()
 download = site.download
-download_playlist = playlist_not_supported('acfun')
+download_playlist = site.download_playlist_by_url

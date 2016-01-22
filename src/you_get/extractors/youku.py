LifeActor/ykdl

commit b0840a5c7f5249dff1215e5cdbdd325081f68c2c

update youku
check data1 instead data

Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

src/you_get/extractors/youku.py
@@ -119,9 +119,9 @@ class Youku(VideoExtractor):
             meta1 = json.loads(get_content(api_url1, headers=youku_headers))
             data = meta['data']
             data1 = meta1['data']
-            assert 'stream' in data
+            assert 'stream' in data1
         except:
-            if 'error' in data:
+            if 'error' in data1:
                 if data['error']['code'] == -202:
                     # Password protected
                     self.password_protected = True
@@ -133,7 +133,7 @@ class Youku(VideoExtractor):
                     data1 = meta1['data']
                     data = meta['data']
                 else:
-                    log.wtf('[Failed] ' + data['error']['note'])
+                    log.wtf('[Failed] ' + data1['error']['note'])
             else:
                 log.wtf('[Failed] Video not found.')
 

commit 75ef9ec08267efe3243147e90d4900805c2b78ba

fix mistake from youku
introduced by: b0840a5c7f5249dff1215e5cdbdd325081f68c2c

Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

src/you_get/extractors/youku.py
@@ -122,7 +122,7 @@ class Youku(VideoExtractor):
             assert 'stream' in data1
         except:
             if 'error' in data1:
-                if data['error']['code'] == -202:
+                if data1['error']['code'] == -202:
                     # Password protected
                     self.password_protected = True
                     self.password = input(log.sprint('Password: ', log.YELLOW))
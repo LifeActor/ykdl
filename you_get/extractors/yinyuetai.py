917fc056d836638575353a5e38e739e6df114821
Zhang Ning <zhangn1985@gmail.com>
2016-05-04 22:06:04 +0800

    support YinYueTai
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- you_get/extractors/yinyuetai.py
+++ you_get/extractors/yinyuetai.py
@@ -1,31 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-__all__ = ['yinyuetai_download', 'yinyuetai_download_by_id']
+from ..extractor import VideoExtractor
+from ..util.html import get_content
+from ..util.match import match1
+from ..util import log
 
-from ..common import *
+import json
 
-def yinyuetai_download_by_id(vid, title=None, output_dir='.', merge=True, info_only=False):
-    video_info = json.loads(get_html('http://www.yinyuetai.com/insite/get-video-info?json=true&videoId=%s' % vid))
-    url_models = video_info['videoInfo']['coreVideoInfo']['videoUrlModels']
-    url_models = sorted(url_models, key=lambda i: i['qualityLevel'])
-    url = url_models[-1]['videoUrl']
-    type = ext = r1(r'\.(flv|mp4)', url)
-    _, _, size = url_info(url)
+class YinYueTai(VideoExtractor):
+    name = 'YinYueTai (音乐台)'
+    supported_stream_types = ['he', 'hd', 'hc' ]
+    def prepare(self):
+        assert self.url or self.vid
 
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
+        if not self.vid:
+            self.vid = match1(self.url, 'http://\w+.yinyuetai.com/video/(\d+)')
 
-def yinyuetai_download(url, output_dir='.', merge=True, info_only=False):
-    id = r1(r'http://\w+.yinyuetai.com/video/(\d+)$', url.split('?')[0])
-    assert id
-    html = get_html(url, 'utf-8')
-    title = r1(r'<meta property="og:title"\s+content="([^"]+)"/>', html)
-    assert title
-    title = parse.unquote(title)
-    title = escape_file_path(title)
-    yinyuetai_download_by_id(id, title, output_dir, merge = merge, info_only = info_only)
+        data = json.loads(get_content('http://ext.yinyuetai.com/main/get-h-mv-info?json=true&videoId={}'.format(self.vid)))
 
-site_info = "YinYueTai.com"
-download = yinyuetai_download
-download_playlist = playlist_not_supported('yinyuetai')
+        if data['error']:
+            log.e('some error happens')
+
+        video_data = data['videoInfo']['coreVideoInfo']
+
+        self.title = video_data['videoName']
+
+        for s in video_data['videoUrlModels']:
+            self.stream_types.append(s['qualityLevel'])
+            self.streams[s['qualityLevel']] = {'container': 'flv', 'video_profile': s['qualityLevelName'], 'src' : [s['videoUrl']], 'size': s['fileSize']}
+
+        self.stream_types = sorted(self.stream_types, key = self.supported_stream_types.index)
+
+site = YinYueTai()

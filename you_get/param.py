bd66a25bd50a44914ce8c6f468dd775f96b33a96
Zhang Ning <zhangn1985@gmail.com>
2016-05-04 08:15:20 +0800

    remove unsupported opts, and update version
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- you_get/param.py
+++ you_get/param.py
@@ -3,34 +3,29 @@ import getopt
 import sys
 from .util import log
 
-version = 'You-Get, a video downloader.'
+version = 'You-Get, a video downloader. Forked form you-get 0.3.34@soimort'
 help = 'Usage: You-Get [OPTION]... [URL]...\n'
 help += '''\nStartup options:
 -V | --version                           Display the version and exit.
 -h | --help                              Print this help and exit.
 '''
 help += '''\nDownload options (use with URLs):
--f | --force                             Force overwriting existed files.
 -i | --info                              Display the information of videos without downloading.
 -u | --url                               Display the real URLs of videos without downloading.
--c | --cookies                           Load NetScape's cookies.txt file.
+-j | --json                              Display info in json format
 -F | --format <STREAM_ID>                Video format code.
 -o | --output-dir <PATH>                 Set the output directory for downloaded videos.
 -p | --player <PLAYER [options]>         Directly play the video with PLAYER like vlc/smplayer.
--x | --http-proxy <HOST:PORT>            Use specific HTTP proxy for downloading.
--y | --extractor-proxy <HOST:PORT>       Use specific HTTP proxy for extracting stream data.
-     --no-proxy                          Don't use any proxy. (ignore $http_proxy)
-     --debug                             Show traceback on KeyboardInterrupt.
+-l | --playlist                          Download as a playlist
 '''
 
 class Param():
 
-    short_opts = 'lVhfiujF:o:p:'
-    opts = ['playlist', 'version', 'help', 'force', 'info', 'url', 'format=', 'output-dir=', 'player=', 'json']
+    short_opts = 'lVhiujF:o:p:'
+    opts = ['playlist', 'version', 'help', 'info', 'url', 'format=', 'output-dir=', 'player=', 'json']
 
     def __init__(self, param_string):
         self.dry_run = False
-        self.force = False
         self.player = None
         self.stream_id = None
         self.info_only = False
@@ -54,8 +49,6 @@ class Param():
                 print(version)
                 print(help)
                 sys.exit()
-            elif o in ('-f', '--force'):
-                self.force = True
             elif o in ('-i', '--info'):
                 self.info_only = True
             elif o in ('-u', '--url'):
@@ -68,8 +61,6 @@ class Param():
                 self.output_dir = a
             elif o in ('-p', '--player'):
                 self.player = a
-            elif o in ('--lang',):
-                self.lang = a
             elif o in ('--json', '-j'):
                 self.json_out = True
             else:
--- you_get/version.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-script_name = 'you-get'
-__version__ = '0.3.34'

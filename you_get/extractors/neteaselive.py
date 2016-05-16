f21db3367044620d93482129903f0c516544ff4d
Zhang Ning <zhangn1985@gmail.com>
2016-05-16 22:12:10 +0800

    add title for cc.163.com
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- you_get/extractors/neteaselive.py
+++ you_get/extractors/neteaselive.py
@@ -12,6 +12,9 @@ class NeteaseLive(VideoExtractor):
         if not self.vid:
             html = get_content(self.url)
             self.vid = match1(html, "anchorCcId : \'([^\']+)")
+            self.title = match1(html, "<title>([^<]+)")
+        else:
+            self.title = self.name + str(self.vid)
 
         info = json.loads(get_content("http://cgi.v.cc.163.com/video_play_url/{}".format(self.vid)))
 

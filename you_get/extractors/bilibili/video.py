335e8ffa0819f6c63430ce1706fcd31823f1451b
Zhang Ning <zhangn1985@gmail.com>
2016-05-26 18:31:09 +0800

    bilibili: support multi stream quality
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- you_get/extractors/bilibili/video.py
+++ you_get/extractors/bilibili/video.py
@@ -22,28 +22,19 @@ def parse_cid_playurl(xml):
 
 class BiliVideo(VideoExtractor):
     name = '哔哩哔哩 (Bilibili)'
-    live = False
+    supported_stream_types = ['超清', '高清', '流畅']
     def prepare(self):
 
         if not self.vid:
             html = get_content(self.url)
             self.vid = match1(html, 'cid=([^&]+)')
             self.title = match1(html, '<title>([^<]+)')
+        for q in self.supported_stream_types:
+            api_url = 'http://interface.bilibili.com/playurl?appkey=' + appkey + '&cid=' + self.vid + '&quality=' + str(3-self.supported_stream_types.index(q))
+            urls, size = parse_cid_playurl(get_content(api_url))
+            ext = 'flv'
 
-        else:
-            if not self.title:
-                self.title = self.name + "-" + self.vid
-
-
-        if self.url:
-            if re.search('live', self.url):
-                self.live = True
-
-        api_url = 'http://interface.bilibili.com/playurl?appkey=' + appkey + '&cid=' + self.vid
-        urls, size = parse_cid_playurl(get_content(api_url))
-        ext = 'flv'
-
-        self.stream_types.append('current')
-        self.streams['current'] = {'container': ext, 'video_profile': 'current', 'src' : urls, 'size': size}
+            self.stream_types.append(q)
+            self.streams[q] = {'container': ext, 'video_profile': q, 'src' : urls, 'size': size}
 
 site = BiliVideo()

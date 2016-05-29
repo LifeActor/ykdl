1bb87bdf49efbbf385e555143caa2ed73bcfc337
Zhang Ning <zhangn1985@gmail.com>
2016-05-29 17:14:05 +0800

    Kuwo: rewrite using VideoExtractor
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- you_get/extractors/kuwo.py
+++ you_get/extractors/kuwo.py
@@ -1,38 +1,25 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-__all__ = ['kuwo_download']
+from you_get.util.match import match1
+from you_get.util.html import get_content
+from you_get.extractor import VideoExtractor
 
-from ..common import *
-import re
+class Kuwo(VideoExtractor):
+    name = 'KuWo (酷我音乐)'
+    supported_stream_types = ['aac', 'mp3']
+    def prepare(self):
+        if not self.vid:
+            self.vid = match1(self.url, 'yinyue/(\d+)')
 
-def kuwo_download_by_rid(rid, output_dir = '.', merge = True, info_only = False):
-    html=get_content("http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_%s"%rid)
-    title=match1(html,r"<name>(.*)</name>")
-    #to get title
-    #format =aac|mp3 ->to get aac format=mp3 ->to get mp3
-    url=get_content("http://antiserver.kuwo.cn/anti.s?format=mp3&rid=MUSIC_%s&type=convert_url&response=url"%rid)
-    songtype, ext, size = url_info(url)
-    print_info(site_info, title, songtype, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir)
+        html = get_content("http://player.kuwo.cn/webmusic/st/getNewMuiseByRid?rid=MUSIC_{}".format(self.vid))
+        self.title = match1(html, "<name>(.*)</name>")
+        self.artist = match1(html, "<artist>(.*)</artist>")
+        for t in self.supported_stream_types:
+            url=get_content("http://antiserver.kuwo.cn/anti.s?format={}&rid=MUSIC_{}&type=convert_url&response=url".format(t, self.vid))
 
-def kuwo_playlist_download(url, output_dir = '.', merge = True, info_only = False):
-    html=get_content(url)
-    matched=set(re.compile("yinyue/(\d+)").findall(html))#reduce duplicated
-    for rid in matched:
-        kuwo_download_by_rid(rid,output_dir,merge,info_only)
-
+            self.stream_types.append(t)
+            self.streams[t] = {'container': t, 'video_profile': 'current', 'src' : [url], 'size': 0}
 
 
-def kuwo_download(url, output_dir = '.', merge = True, info_only = False):
-    if "www.kuwo.cn/yinyue" in url:
-        rid=match1(url,'yinyue/(\d+)')
-        kuwo_download_by_rid(rid,output_dir, merge, info_only)
-    else:
-        kuwo_playlist_download(url,output_dir,merge,info_only)
-
-site_info = "kuwo.cn"
-download = kuwo_download
-# download_playlist = playlist_not_supported("kugou")
-# download_playlist=playlist_not_supported("kuwo")
-download_playlist=kuwo_playlist_download
+site = Kuwo()

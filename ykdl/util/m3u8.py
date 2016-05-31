5bb704e677abb199d2e13f566d45efc05cd5269d
Zhang Ning <zhangn1985@gmail.com>
2016-05-31 22:36:39 +0800

    m3u8: workaround for m3u8 live stream
    when m3u8 is live stream, is_endlist is False
    we need to re-request m3u8 link
    
    but if the download speed is fast, time of two m3u8 link is overlapped
    
    in order to workaround time overlapped, we need to simulate player by using sleep.
    
    but this can't be prefect!
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/util/m3u8.py
+++ ykdl/util/m3u8.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+
 try:
 
     import m3u8
@@ -17,9 +19,34 @@ try:
     def load_m3u8(url):
         urls = []
         m =  m3u8.load(url)
-        for seg in m.segments:
-            urls.append(seg.absolute_uri)
-        return urls
+        now = d = 0
+        if m.is_endlist:
+            for seg in m.segments:
+                urls.append(seg.absolute_uri)
+            return urls
+        else:
+            """
+            the stream is live stream. so we use sleep to simulate player. but not perfact!
+            """
+            i = 0
+            while True:
+                if i < len(m.segments):
+                    delta = d -( time.time() - now)
+                    if (delta) > 0:
+                        time.sleep((d - (time.time() - now)) / 1000)
+                    segurl = m.segments[i].absolute_uri
+                    now = time.time()
+                    d = m.segments[i].duration * 1000
+                    i += 1
+                    yield segurl
+                else:
+                    i = 0
+                    delta = d -( time.time() - now) - len(m.segments) * 100 #workaround
+                    if (delta) > 0:
+                        time.sleep((d - (time.time() - now)) / 1000)
+                    m = m3u8.load(url)
+                    now = time.time()
+                    d = 0
 
 except:
     def load_m3u8_playlist(url):

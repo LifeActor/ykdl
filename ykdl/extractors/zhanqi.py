8535e5f444853c4a96f5052d4fbf5e4cdf2206c2
Zhang Ning <zhangn1985@gmail.com>
2016-06-16 20:22:25 +0800

    zhanqi: fix bug, and simplified, because m3u8
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/zhanqi.py
+++ ykdl/extractors/zhanqi.py
@@ -23,25 +23,14 @@ class Zhanqi(VideoExtractor):
         elif not video_type == 'VOD':
             NotImplementedError('Unknown_video_type')
 
-        self.title = match1(html, '<title>([^<]+)')
+        self.title = match1(html, '<title>([^<]+)').split("_")[0]
         if self.live:
             rtmp_id = match1(html, 'videoId":"([^"]+)"').replace('\\/','/')
             real_url = self.live_base+'/'+rtmp_id+'.m3u8'
             self.stream_types, self.streams = load_m3u8_playlist(real_url)
         else:
-            vod_m3u8_request = self.vod_base + match1(html, 'VideoID":"([^"]+)').replace('\\/','/')
-            vod_m3u8 = get_content(vod_m3u8_request)
-            part_url = re.findall(r'(/[^#]+)\.ts',vod_m3u8)
-            real_url = []
-            for i in part_url:
-                i = self.vod_base + i + ".ts"
-                real_url.append(i)
-            type_ = ''
-            size = 0
-            for url in real_url:
-                _, type_, temp = url_info(url)
-                size += temp or 0
+            vod_m3u8 = self.vod_base + '/' + match1(html, 'VideoID":"([^"]+)').replace('\\/','/')
             self.stream_types.append('current')
-            self.streams['current'] = {'container': 'flv', 'video_profile': 'current', 'src' : real_url, 'size': size}
+            self.streams['current'] = {'container': 'm3u8', 'video_profile': 'current', 'src' : [vod_m3u8], 'size': 0}
 
 site = Zhanqi()

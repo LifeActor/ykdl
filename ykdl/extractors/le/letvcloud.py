5dd3e37a5720b0dede9c8e89474aa4ad98c4f806
Zhang Ning <zhangn1985@gmail.com>
2016-06-19 15:28:37 +0800

    letvcloud: support video name
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/le/letvcloud.py
+++ ykdl/extractors/le/letvcloud.py
@@ -16,7 +16,6 @@ class Letvcloud(VideoExtractor):
         #ran = float('0.' + str(random.randint(0, 9999999999999999))) # For ver 2.1
         #str2Hash = 'cfflashformatjsonran{ran}uu{uu}ver2.2vu{vu}bie^#@(%27eib58'.format(vu = vu, uu = uu, ran = ran)  #Magic!/ In ver 2.1
         vu, uu = self.vid
-        self.title = "LeTVcloud_{}_{}".format(vu, uu)
         argumet_dict ={'cf' : 'flash', 'format': 'json', 'ran': str(int(time.time())), 'uu': str(uu),'ver': '2.2', 'vu': str(vu), }
         sign_key = '2f9d6924b33a165a6d8b5d3d42f4f987'  #ALL YOUR BASE ARE BELONG TO US
         str2Hash = ''.join([i + argumet_dict[i] for i in sorted(argumet_dict)]) + sign_key
@@ -24,11 +23,17 @@ class Letvcloud(VideoExtractor):
         html = get_content('http://api.letvcloud.com/gpc.php?' + '&'.join([i + '=' + argumet_dict[i] for i in argumet_dict]) + '&sign={sign}'.format(sign = sign), charset= 'utf-8')
         info = json.loads(html)
         assert info['code'] == 0, info['message']
+        video_name = info['data']['video_info']['video_name']
+        if '.' in video_name:
+            ext = video_name.split('.')[-1]
+            self.title = video_name[0:-len(ext)-1]
+        else:
+            ext = 'mp4'
+            self.title = video_name
         available_stream_type = info['data']['video_info']['media'].keys()
         for stream in self.supported_stream_types:
             if stream in available_stream_type:
                 urls = [base64.b64decode(info['data']['video_info']['media'][stream]['play_url']['main_url']).decode("utf-8")]
-                ext = 'mp4'
                 self.stream_types.append(stream)
                 self.streams[stream] = {'container': ext, 'video_profile': stream, 'src': urls, 'size' : 0}
 
@@ -36,7 +41,7 @@ class Letvcloud(VideoExtractor):
 
         if self.url and not self.vid:
             #maybe error!!
-            self.vid = (vu, uu) = matchall(self.url, ["vu=([^&]+)","uu=([^&]+)"])        
+            self.vid = (vu, uu) = matchall(self.url, ["vu=([^&]+)","uu=([^&]+)"])
         self.letvcloud_download_by_vu()
 
 site = Letvcloud()

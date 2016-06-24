7009bf2b781473295965388bed19323b2935a999
Zhang Ning <zhangn1985@gmail.com>
2016-06-24 20:48:12 +0800

    youku: update and remove buggy playlist
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/youku.py
+++ ykdl/extractors/youku.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ..util.html import get_content, fake_headers
-from ..util.match import match1, matchall
+from ykdl.util.html import get_content, add_header
+from ykdl.util.match import match1, matchall
 from .youkubase import YoukuBase
 from .youkujs import install_acode
 from ykdl.compact import HTTPSHandler, build_opener, HTTPCookieProcessor, install_opener
@@ -11,35 +11,10 @@ import time
 import json
 import ssl
 
-
-youku_headers = fake_headers
-youku_headers['Referer'] = 'v.youku.com'
-
 class Youku(YoukuBase):
     name = u"优酷 (Youku)"
 
     ct = 12
-
-    def download_playlist(self, url, param):
-        self.url = url
-
-        try:
-            playlist_id = match1(self.url, 'youku\.com/playlist_show/id_([a-zA-Z0-9=]+)')
-            assert playlist_id
-
-            video_page = get_content('http://www.youku.com/playlist_show/id_%s' % playlist_id, headers=youku_headers)
-            videos = matchall(video_page, ['a href="(http://v\.youku\.com/[^?"]+)'])
-
-            for extra_page_url in matchall(video_page, ['href="(http://www\.youku\.com/playlist_show/id_%s_[^?"]+)' % playlist_id]):
-                extra_page = get_content(extra_page_url, headers=youku_headers)
-                videos += matchall(extra_page, ['a href="(http://v\.youku\.com/[^?"]+)'])
-
-        except:
-            video_page = get_content(url, headers=youku_headers)
-            videos = videos = matchall(video_page, ['a href="(http://v\.youku\.com/[^?"]+)'])
-
-        for video in videos:
-            self.download(video, param)
 
     def setup(self):
         # Hot-plug cookie handler
@@ -50,7 +25,7 @@ class Youku(YoukuBase):
         opener.addheaders = [('Cookie','__ysuid=%d' % time.time())]
         install_opener(opener)
 
-
+        add_header('Referer', 'v.youku.com')
 
         install_acode('4', '1', 'b4et', 'boa4', 'o0b', 'poz')
         if self.url and not self.vid:
@@ -63,8 +38,8 @@ class Youku(YoukuBase):
         api_url = 'http://play.youku.com/play/get.json?vid=%s&ct=12' % self.vid
         api_url1 = 'http://play.youku.com/play/get.json?vid=%s&ct=10' % self.vid
         try:
-            meta = json.loads(get_content(api_url, headers=youku_headers))
-            meta1 = json.loads(get_content(api_url1, headers=youku_headers))
+            meta = json.loads(get_content(api_url))
+            meta1 = json.loads(get_content(api_url1))
             data = meta['data']
             data1 = meta1['data']
             assert 'stream' in data1
@@ -76,8 +51,8 @@ class Youku(YoukuBase):
                     self.password = input(log.sprint('Password: ', log.YELLOW))
                     api_url += '&pwd={}'.format(self.password)
                     api_url1 += '&pwd={}'.format(self.password)
-                    meta1 = json.loads(get_content(api_url1, headers=youku_headers))
-                    meta = json.loads(get_content(api_url, headers=youku_headers))
+                    meta1 = json.loads(get_content(api_url1))
+                    meta = json.loads(get_content(api_url))
                     data1 = meta1['data']
                     data = meta['data']
                 else:

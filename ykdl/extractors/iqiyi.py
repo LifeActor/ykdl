241e99dd00a7f780c04bbf9b521e9bbbc395c923
Zhang Ning <zhangn1985@gmail.com>
2016-06-29 18:39:38 +0800

    iqiyi: support more stream quality
    algorism form @ERioK
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/iqiyi.py
+++ ykdl/extractors/iqiyi.py
@@ -4,11 +4,11 @@
 from ykdl.util.html import get_content
 from ykdl.util.match import matchall, match1
 from ykdl.extractor import VideoExtractor
+from ykdl.compact import compact_bytes
 
 import json
 import time
-
-from .iqiyi_sc import gen_sc
+import hashlib
 
 '''
 com.qiyi.player.core.model.def.DefinitonEnum
@@ -25,7 +25,7 @@ bid meaning for quality
 
 class Iqiyi(VideoExtractor):
     name = u"爱奇艺 (Iqiyi)"
-
+    '''
     supported_stream_types = [ 'high', 'standard']
 
     stream_to_bid = {  '4k': 10, 'fullhd' : 5, 'suprt-high' : 4, 'super' : 3, 'high' : 2, 'standard' :1, 'topspeed' :96}
@@ -33,15 +33,19 @@ class Iqiyi(VideoExtractor):
     stream_2_id = {  '4k': 'BD', 'fullhd' : 'FD', 'suprt-high' : 'OD', 'super' : 'TD', 'high' : 'HD', 'standard' :'SD', 'topspeed' :'LD'}
 
     stream_2_profile = {  '4k': u'4k', 'fullhd' : u'全高清', 'suprt-high' : u'超高清', 'super' : u'超清', 'high' : u'高清', 'standard' : u'标清', 'topspeed' : u'急速'}
-
+    '''
     ids = ['BD', 'FD', 'OD', 'TD', 'HD', 'SD', 'LD']
+    vd_2_id = {21: 'TD', 2: 'HD', 4: 'FD', 17: 'BD', 96: 'LD', 1: 'SD'}
+    vd_2_profile = {21: u'超清', 2: u'高清', 4: u'超高清', 17: u'全高清', 96: u'流畅', 1: u'标清'}
 
-    def getVMS(self, rate):
+    def getVMS(self):
         tvid, vid = self.vid
         t = int(time.time() * 1000)
-        sc = gen_sc(tvid, t).decode('utf-8')
-        vmsreq= 'http://cache.m.iqiyi.com/jp/tmts/{}/{}/?platForm=h5&rate={}&tvid={}&vid={}&cupid=qc_100001_100186&type=mp4&olimit=0&agenttype=13&src=d846d0c32d664d32b6b54ea48997a589&sc={}&t={}&__jsT=null'.format(tvid, vid, rate, tvid, vid, sc, t - 7)
-        return json.loads(get_content(vmsreq)[13:])
+        src = '76f90cbd92f94a2e925d83e8ccd22cb7'
+        key = 'd5fb4bd9d50c4be6948c97edd7254b0e'
+        sc = hashlib.new('md5', compact_bytes(str(t) + key  + vid, 'utf-8')).hexdigest()
+        vmsreq= url = 'http://cache.m.iqiyi.com/tmts/{0}/{1}/?t={2}&sc={3}&src={4}'.format(tvid,vid,t,sc,src)
+        return json.loads(get_content(vmsreq))
 
     def prepare(self):
 
@@ -55,16 +59,17 @@ class Iqiyi(VideoExtractor):
             tvid = match1(html, 'data-player-tvid="([^"]+)"') or match1(self.url, 'tvid=([^&]+)')
             videoid = match1(html, 'data-player-videoid="([^"]+)"') or match1(self.url, 'vid=([^&]+)')
             self.vid = (tvid, videoid)
+            self.title = match1(html, '<title>([^<]+)').split('-')[0]
 
-        for stream in self.supported_stream_types:
-            info = self.getVMS(self.stream_to_bid[stream])
-            if info["code"] == "A00000":
-                stream_id = self.stream_2_id[stream]
-                stream_profile = self.stream_2_profile[stream]
-                self.title = info['data']['playInfo']['vn']
-                self.stream_types.append(stream_id)
-                self.streams[stream_id] = {'container': 'mp4', 'video_profile': stream_profile, 'src' : [info['data']['m3u']], 'size' : 0}
+        info = self.getVMS()
+        assert info['code'] == 'A00000', 'can\'t play this video'
 
+        for stream in info['data']['vidl']:
+           stream_id = self.vd_2_id[stream['vd']]
+           stream_profile = self.vd_2_profile[stream['vd']]
+           self.stream_types.append(stream_id)
+           self.streams[stream_id] = {'video_profile': stream_profile, 'container': 'm3u8', 'src': [stream['m3u']], 'size' : 0}
+        self.stream_types = sorted(self.stream_types, key = self.ids.index)
     def prepare_list(self):
         html = get_content(self.url)
 

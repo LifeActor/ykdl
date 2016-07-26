5dead12622f22ab364b6479fec8025f0f0043647
Zhang Ning <zhangn1985@gmail.com>
2016-07-26 20:57:05 +0800

    update huomao, due to it updated
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/huomao.py
+++ ykdl/extractors/huomao.py
@@ -16,14 +16,14 @@ class HuomaoTv(VideoExtractor):
 
     stream_2_profile = {'BD': u"原画", 'TD': u'超清', 'HD': u'高清', 'SD': u'标清' }
 
-    live_base = "http://www.huomaotv.cn/swf/live_data"
+    live_base = "http://www.huomao.com/swf/live_data"
 
     def prepare(self):
         info = VideoInfo(self.name, True)
         html = get_content(self.url)
         info.title = match1(html, '<title>([^<]+)').split('/')[0]
 
-        video_name = match1(html, 'video_name = \'([^\']+)')
+        video_name = match1(html, 'getFlash\("[0-9]+","([^"]+)')
         params = { 'streamtype':'live',
                    'VideoIDS': video_name,
                    'cdns' : '1'
@@ -31,6 +31,7 @@ class HuomaoTv(VideoExtractor):
         form = urlencode(params)
         content = get_content(self.live_base,data=compact_bytes(form, 'utf-8'),charset = 'utf-8')
         stream_data = json.loads(content)
+
         assert stream_data["roomStatus"] == "1", "The live stream is not online! "
         for stream in stream_data["streamList"]:
             if stream['default'] == 1:

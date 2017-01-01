8f69b890101cbad32b12a8324cddea73db4d4ebe
Valdemar Erk <valdemarerk@gmail.com>
2017-01-02 00:40:26 +0100

    Fix for some streams on panda.tv.

--- ykdl/extractors/panda.py
+++ ykdl/extractors/panda.py
@@ -10,7 +10,7 @@ import json
 class Panda(VideoExtractor):
     name = u'熊猫TV (Panda)'
 
-    live_base = "http://pl3.live.panda.tv/live_panda/{}.flv"
+    live_base = "http://pl{}.live.panda.tv/live_panda/{}.flv"
     api_url = "http://www.panda.tv/api_room?roomid={}"
 
     def prepare(self):
@@ -22,12 +22,13 @@ class Panda(VideoExtractor):
         stream_data = json.loads(content)
         assert stream_data['data']['videoinfo']['status'] == '2', u"error: (⊙o⊙)主播暂时不在家，看看其他精彩直播吧！"
         room_key = stream_data['data']['videoinfo']['room_key']
+        plflag = stream_data['data']['videoinfo']['plflag'].split('_')[1]
         info.title = stream_data['data']['roominfo']['name']
         info.artist = stream_data['data']['hostinfo']['name']
 
 
         info.stream_types.append('current')
-        info.streams['current'] = {'container': 'flv', 'video_profile': 'current', 'src' : [self.live_base.format(room_key)], 'size': float('inf')}
+        info.streams['current'] = {'container': 'flv', 'video_profile': 'current', 'src' : [self.live_base.format(plflag, room_key)], 'size': float('inf')}
         return info
 
 site = Panda()

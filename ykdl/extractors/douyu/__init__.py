8574dc5383d8963ec54b4d2a32ec4fa40d9850ee
Zhang Ning <zhangn1985@gmail.com>
2017-01-07 12:39:33 +0800

    support douyu video by using mobile api
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/douyu/__init__.py
+++ ykdl/extractors/douyu/__init__.py
@@ -3,7 +3,7 @@
 import re
 
 def get_extractor(url):
-    if re.search("v.douyu", url):
+    if re.search("v.douyu", url) or re.search("vmobile.douyu", url):
         from . import video as s
         return s.site
     else:
--- ykdl/extractors/douyu/video.py
+++ ykdl/extractors/douyu/video.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ykdl.util.html import get_content
+from ykdl.util.html import get_content, add_header
 from ykdl.util.match import match1, matchall
 from ykdl.extractor import VideoExtractor
 from ykdl.videoinfo import VideoInfo
@@ -17,28 +17,18 @@ class DouyutvVideo(VideoExtractor):
 
     def prepare(self):
         info = VideoInfo(self.name)
+        add_header('X-Requested-With', 'XMLHttpRequest')
         if self.url:
             self.vid = match1(self.url, 'show/(.*)')
 
-        json_request_url = "https://v.douyu.com/api/swf/getvideourl/{}".format(self.vid)
-        tt = int(time.time())
-        did = uuid.uuid4().hex.upper()
-        sign_content = '{room_id}{did}A12Svb&%1UUmf@hC{tt}'.format(room_id = self.vid, did = did, tt = tt)
-        sign = hashlib.md5(sign_content.encode('utf-8')).hexdigest()
-        params = {
-            'sign': sign,
-            'vid': self.vid,
-            'did': did,
-            'tt': tt
-        }
-        form = urlencode(params)
+        json_request_url = "https://vmobile.douyu.com/video/getInfo?vid={}".format(self.vid)
 
-        video_data = json.loads(get_content(json_request_url, data=compact_bytes(form,'utf-8')))
+        video_data = json.loads(get_content(json_request_url))
         assert video_data['error'] == 0, video_data
-        real_url = video_data['data']['thumb_video']['normal']['url']
+        real_url = video_data['data']['video_url']
         info.stream_types.append('current')
         info.streams['current'] = {'container': 'flv', 'video_profile': 'current', 'src' : [real_url], 'size': 0}
-        info.title = video_data['data']['title']
+        info.title = self.name + '_' + self.vid
         return info
 
 site = DouyutvVideo()

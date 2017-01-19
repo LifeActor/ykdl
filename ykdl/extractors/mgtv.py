b186502373741623f4032573a48a2032cd80f4f5
Zhang Ning <zhangn1985@gmail.com>
2017-01-19 20:42:01 +0800

    mgtv: fix due to new api
    close: #51
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/mgtv.py
+++ ykdl/extractors/mgtv.py
@@ -6,7 +6,6 @@ from ykdl.util.match import match1, matchall
 from ykdl.extractor import VideoExtractor
 from ykdl.videoinfo import VideoInfo
 
-from random import randint
 import json
 
 class Hunantv(VideoExtractor):
@@ -21,18 +20,18 @@ class Hunantv(VideoExtractor):
         if self.url and not self.vid:
             self.vid = match1(self.url, "/([0-9]+).html")
 
-        rn = randint(0, 99999999)
-        api_url = 'http://v.api.hunantv.com/player/video?video_id={}&random={}'.format(self.vid,rn)
+        api_url = 'http://pcweb.api.mgtv.com/player/video?video_id={}'.format(self.vid)
         meta = json.loads(get_content(api_url))
 
-        assert meta['status'] == 200, '[failed] status: {}, msg: {}'.format(meta['status'],meta['msg'])
+        assert meta['code'] == 200, '[failed] status: {}, msg: {}'.format(meta['status'],meta['msg'])
         assert meta['data'], '[Failed] Video not found.'
 
         data = meta['data']
 
         info.title = data['info']['title']
+        domain = data['stream_domain'][0]
         for lstream in data['stream']:
-            url = json.loads(get_content(lstream['url']))['info']
+            url = json.loads(get_content(domain + lstream['url']))['info']
             info.streams[self.profile_2_types[lstream['name']]] = {'container': 'm3u8', 'video_profile': lstream['name'], 'src' : [url]}
             info.stream_types.append(self.profile_2_types[lstream['name']])
         info.stream_types= sorted(info.stream_types, key = self.supported_stream_types.index)

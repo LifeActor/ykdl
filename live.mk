c08ac267b53ef0536aa1a0d5a5889685ba469306
Zhang Ning <zhangn1985@gmail.com>
2017-03-30 20:12:56 +0800

    update test makefile
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- live.mk
+++ live.mk
@@ -1,34 +1,36 @@
 test_live: test_bobo test_douyu test_huomao test_longzhu test_panda test_zhanqi test_bililive test_huyalive test_lelive test_cc test_qqlive
 
+PYTHON ?= python3
+
 test_bobo:
-	${PYTHON} bin/ykdl.py -i http://www.bobo.com/10003822?f=pHome.Hot_anchor.1
+	${PYTHON} -m cykdl -i http://www.bobo.com/10003822?f=pHome.Hot_anchor.1
 
 test_douyu:
-	${PYTHON} bin/ykdl.py -i http://www.douyu.com/58428
+	${PYTHON} -m cykdl -i http://www.douyu.com/58428
 
 test_huomao:
-	${PYTHON} bin/ykdl.py -i http://www.huomaotv.cn/live/845
+	${PYTHON} -m cykdl -i http://www.huomaotv.cn/live/845
 
 test_longzhu:
-	${PYTHON} bin/ykdl.py -i http://star.longzhu.com/133097?from=challcontent
+	${PYTHON} -m cykdl -i http://star.longzhu.com/133097?from=challcontent
 
 test_panda:
-	${PYTHON} bin/ykdl.py -i http://www.panda.tv/60995
+	${PYTHON} -m cykdl -i http://www.panda.tv/60995
 
 test_zhanqi:
-	${PYTHON} bin/ykdl.py -i https://www.zhanqi.tv/naigege
+	${PYTHON} -m cykdl -i https://www.zhanqi.tv/naigege
 
 test_bililive:
-	${PYTHON} bin/ykdl.py -i http://live.bilibili.com/3
+	${PYTHON} -m cykdl -i http://live.bilibili.com/3
 
 test_huyalive:
-	${PYTHON} bin/ykdl.py -i http://www.huya.com/lengsimo
+	${PYTHON} -m cykdl -i http://www.huya.com/lengsimo
 
 test_lelive:
-	${PYTHON} bin/ykdl.py -i http://live.le.com/lunbo/play/index.shtml?channel=224
+	${PYTHON} -m cykdl -i http://live.le.com/lunbo/play/index.shtml?channel=224
 
 test_cc:
-	${PYTHON} bin/ykdl.py -i http://cc.163.com/30348786/
+	${PYTHON} -m cykdl -i http://cc.163.com/30348786/
 
 test_qqlive:
-	${PYTHON} bin/ykdl.py -i http://live.qq.com/10001075
+	${PYTHON} -m cykdl -i http://live.qq.com/10001075
--- video.mk
+++ video.mk
@@ -2,80 +2,82 @@ test_video1: test_youku test_acfun test_bilibili test_ifeng test_163m test_sohut
 test_video2: test_iqilu test_iqiyi test_joy test_ku6 test_kuwo test_sina test_qq test_sohumy test_baomihua
 test_video3: test_xiami test_yinyuetai test_baidu test_douban test_huya test_163v test_le test_mgtv
 
+PYTHON ?= python3
+
 test_youku:
-	${PYTHON} bin/ykdl.py -i http://v.youku.com/v_show/id_XMTYwMDIxNDI2MA==.html
+	${PYTHON} -m cykdl -i http://v.youku.com/v_show/id_XMTYwMDIxNDI2MA==.html
 
 test_acfun:
-	${PYTHON} bin/ykdl.py -i http://www.acfun.tv/v/ac3101039
+	${PYTHON} -m cykdl -i http://www.acfun.tv/v/ac3101039
 
 test_bilibili:
-	${PYTHON} bin/ykdl.py -i http://www.bilibili.com/video/av1358885/
+	${PYTHON} -m cykdl -i http://www.bilibili.com/video/av1358885/
 
 test_baomihua:
-	${PYTHON} bin/ykdl.py -i http://www.baomihua.com/user/24204_36300935
+	${PYTHON} -m cykdl -i http://www.baomihua.com/user/24204_36300935
 
 test_cctv:
-	${PYTHON} bin/ykdl.py -i http://tv.cctv.com/2016/06/08/VIDEa0Y5V5HY9MLeoVM5tcQC160608.shtml -t 300
+	${PYTHON} -m cykdl -i http://tv.cctv.com/2016/06/08/VIDEa0Y5V5HY9MLeoVM5tcQC160608.shtml -t 300
 
 test_ifeng:
-	${PYTHON} bin/ykdl.py -i 'http://v.ifeng.com/fhlbt/special/20160606/index.shtml#01881385-2b2c-11e6-bc61-002590c2aaeb'
+	${PYTHON} -m cykdl -i 'http://v.ifeng.com/fhlbt/special/20160606/index.shtml#01881385-2b2c-11e6-bc61-002590c2aaeb'
 
 test_iqilu:
-	${PYTHON} bin/ykdl.py -i http://v.iqilu.com/shpd/rmxf/2016/0607/4332820.html
+	${PYTHON} -m cykdl -i http://v.iqilu.com/shpd/rmxf/2016/0607/4332820.html
 
 test_iqiyi:
-	${PYTHON} bin/ykdl.py -i http://www.iqiyi.com/v_19rrle48gg.html
+	${PYTHON} -m cykdl -i http://www.iqiyi.com/v_19rrle48gg.html
 
 test_joy:
-	${PYTHON} bin/ykdl.py -i http://www.joy.cn/video?resourceId=60239051
+	${PYTHON} -m cykdl -i http://www.joy.cn/video?resourceId=60239051
 
 test_ku6:
-	${PYTHON} bin/ykdl.py -i http://v.ku6.com/special/show_6615055/ng4jKVcB-zw1r8wRYEtTMA...html?hpsrc=1_25_1_1_0
+	${PYTHON} -m cykdl -i http://v.ku6.com/special/show_6615055/ng4jKVcB-zw1r8wRYEtTMA...html?hpsrc=1_25_1_1_0
 
 test_kuwo:
-	${PYTHON} bin/ykdl.py -i http://www.kuwo.cn/yinyue/7119332?catalog=yueku2016
+	${PYTHON} -m cykdl -i http://www.kuwo.cn/yinyue/7119332?catalog=yueku2016
 
 test_lizhi:
-	${PYTHON} bin/ykdl.py -i http://www.lizhi.fm/202840/29101368624039686
+	${PYTHON} -m cykdl -i http://www.lizhi.fm/202840/29101368624039686
 
 test_sina:
-	${PYTHON} bin/ykdl.py -i 'http://video.sina.com.cn/ent/#250623748' -t 300
+	${PYTHON} -m cykdl -i 'http://video.sina.com.cn/ent/#250623748' -t 300
 
 test_xiami:
-	${PYTHON} bin/ykdl.py -li http://www.xiami.com/album/2100285370?spm=a1z1s.3057849.0.0.hAuVwv
+	${PYTHON} -m cykdl -li http://www.xiami.com/album/2100285370?spm=a1z1s.3057849.0.0.hAuVwv
 
 test_yinyuetai:
-	${PYTHON} bin/ykdl.py -i http://v.yinyuetai.com/video/2591456?f=SY-MKDT-MVSB-1
+	${PYTHON} -m cykdl -i http://v.yinyuetai.com/video/2591456?f=SY-MKDT-MVSB-1
 
 test_baidu:
-	${PYTHON} bin/ykdl.py -li http://music.baidu.com/album/266327865?pst=shoufa
+	${PYTHON} -m cykdl -li http://music.baidu.com/album/266327865?pst=shoufa
 
 test_douban:
-	${PYTHON} bin/ykdl.py -li https://music.douban.com/artists/player/?sid=660498,647629,647625,633870,622482,600594,589516,588385,583322,580114,576350
+	${PYTHON} -m cykdl -li https://music.douban.com/artists/player/?sid=660498,647629,647625,633870,622482,600594,589516,588385,583322,580114,576350
 
 test_huya:
-	${PYTHON} bin/ykdl.py -i http://v.huya.com/play/2209082.html
+	${PYTHON} -m cykdl -i http://v.huya.com/play/2209082.html
 
 test_le:
-	${PYTHON} bin/ykdl.py -i http://www.le.com/ptv/vplay/26859747.html -t 300
+	${PYTHON} -m cykdl -i http://www.le.com/ptv/vplay/26859747.html -t 300
 
 test_163v:
-	${PYTHON} bin/ykdl.py -i http://v.163.com/paike/VBI038VCL/VBNERA654.html
+	${PYTHON} -m cykdl -i http://v.163.com/paike/VBI038VCL/VBNERA654.html
 
 test_163m:
-	${PYTHON} bin/ykdl.py -li http://music.163.com/playlist?id=396542983
+	${PYTHON} -m cykdl -li http://music.163.com/playlist?id=396542983
 
 test_qq:
-	${PYTHON} bin/ykdl.py -i http://v.qq.com/cover/q/qsm7nxzwbnzc4dp.html?vid=m0305m0ur33
+	${PYTHON} -m cykdl -i http://v.qq.com/cover/q/qsm7nxzwbnzc4dp.html?vid=m0305m0ur33
 
 test_sohutv:
-	${PYTHON} bin/ykdl.py -i http://tv.sohu.com/20160607/n453456746.shtml
+	${PYTHON} -m cykdl -i http://tv.sohu.com/20160607/n453456746.shtml
 
 test_sohumy:
-	${PYTHON} bin/ykdl.py -i http://my.tv.sohu.com/pl/9090402/84077110.shtml
+	${PYTHON} -m cykdl -i http://my.tv.sohu.com/pl/9090402/84077110.shtml
 
 test_mgtv:
-	${PYTHON} bin/ykdl.py -i http://www.mgtv.com/v/2/293140/c/3269011.html
+	${PYTHON} -m cykdl -i http://www.mgtv.com/v/2/293140/c/3269011.html
 
 test_tudou:
-	${PYTHON} bin/ykdl.py -i http://www.tudou.com/listplay/91csQ5vcKbg/lwLGzPNUWuQ/
+	${PYTHON} -m cykdl -i http://www.tudou.com/listplay/91csQ5vcKbg/lwLGzPNUWuQ/

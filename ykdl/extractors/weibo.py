867ae89fbae5d2a3e072c2971b16fd007ff80bf0
Zhang Ning <zhangn1985@gmail.com>
2017-04-01 09:16:42 +0800

    weibo: is it a fix?
    for: #106
    
    due to weibo video url can't download without cookies
    I have to use mobile UA to download.
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/weibo.py
+++ ykdl/extractors/weibo.py
@@ -10,8 +10,8 @@ class Weibo(SimpleExtractor):
     def __init__(self):
         SimpleExtractor.__init__(self)
         add_header('User-Agent', 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36')
-        self.url_pattern = '<video src="(.*?)\"\W'
-        self.title_pattern = '<meta name="description" content="([\s\S]*?)\"\W'
+        self.url_pattern = 'data-url=\"([^\"]+)'
+        self.title_pattern = '<title>([^<]+)</'
 
     def l_assert(self):
         self.url = 'http://video.weibo.com/show?fid=1034:' + self.url[-32:] + '&type=mp4'

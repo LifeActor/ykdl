6ab84e309de69c6ad503158b7d679fbe6aa7109e
Zhang Ning <zhangn1985@gmail.com>
2017-04-06 20:34:30 +0800

    update ifeng
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/ifeng/__init__.py
+++ ykdl/extractors/ifeng/__init__.py
@@ -6,8 +6,8 @@ import re
 def get_extractor(url):
     if re.search("video", url):
         from . import video as s
-    elif re.search("news", url):
-        from . import news as s
     elif re.search("gongkaike", url):
         from . import gongkaike as s
+    else:
+        from . import news as s
     return s.site

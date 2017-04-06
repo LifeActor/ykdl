53de9e6b7579f2b06697dc3bdb734c4aac286756
Zhang Ning <zhangn1985@gmail.com>
2017-04-06 20:32:01 +0800

    support ifeng gongkaike
    close: #82
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/ifeng/__init__.py
+++ ykdl/extractors/ifeng/__init__.py
@@ -6,6 +6,8 @@ import re
 def get_extractor(url):
     if re.search("video", url):
         from . import video as s
-    else:
+    elif re.search("news", url):
         from . import news as s
+    elif re.search("gongkaike", url):
+        from . import gongkaike as s
     return s.site

00079285bfc48b18f248517771965cf9580d6439
Zhang Ning <zhangn1985@gmail.com>
2017-04-16 09:08:42 +0800

    youku: update due to api changed
    close: #110 #90
    thanks to @rosynirvana
    
    passwd not supported
    urls integrity not checked.
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/youku.py
+++ ykdl/extractors/youku.py
@@ -4,31 +4,50 @@
 from ykdl.util.html import get_content, add_header
 from ykdl.util.match import match1, matchall
 from ykdl.util import log
-from .youkubase import YoukuBase
-from .youkujs import install_acode
-from ykdl.compact import HTTPSHandler, build_opener, HTTPCookieProcessor, install_opener
+from ykdl.extractor import VideoExtractor
+from ykdl.videoinfo import VideoInfo
 
 import time
 import json
-import ssl
 
-class Youku(YoukuBase):
+supported_stream_code = [ 'mp4hd3', 'hd3', 'mp4hd2', 'hd2', 'mp4hd', 'mp4', 'flvhd', 'flv', '3gphd' ]
+ids = ['BD', 'TD', 'HD', 'SD', 'LD']
+stream_code_to_id = {
+    'mp5hd3': 'BD',
+    'mp4hd3': 'BD',
+    'hd3'   : 'BD',
+    'mp5hd2': 'TD',
+    'mp4hd2': 'TD',
+    'hd2'   : 'TD',
+    'mp5hd' : 'HD',
+    'mp4hd' : 'HD',
+    'mp4'   : 'HD',
+    'flvhd' : 'SD',
+    'flv'   : 'SD',
+    '3gphd' : 'LD'
+}
+stream_code_to_profiles = {
+    'BD' : u'1080p',
+    'TD' : u'超清',
+    'HD' : u'高清',
+    'SD' : u'标清',
+    'LD' : u'标清（3GP）'
+}
+id_to_container = {
+    'BD' : 'flv',
+    'TD' : 'flv',
+    'HD' : 'mp4',
+    'SD' : 'flv',
+    'LD' : 'mp4'
+}
+
+class Youku(VideoExtractor):
     name = u"优酷 (Youku)"
 
-    ct = 12
 
-    def setup(self, info):
-        # Hot-plug cookie handler
-        ssl_context = HTTPSHandler(
-            context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
-        cookie_handler = HTTPCookieProcessor()
-        opener = build_opener(ssl_context, cookie_handler)
-        opener.addheaders = [('Cookie','__ysuid=%d' % time.time())]
-        install_opener(opener)
+    def prepare(self):
+        info = VideoInfo(self.name)        
 
-        add_header('Referer', 'v.youku.com')
-
-        install_acode('4', '1', 'b4et', 'boa4', 'o0b', 'poz')
         if self.url and not self.vid:
              self.vid = match1(self.url, 'youku\.com/v_show/id_([a-zA-Z0-9=]+)' ,\
                                          'player\.youku\.com/player\.php/sid/([a-zA-Z0-9=]+)/v\.swf',\
@@ -36,40 +55,26 @@ class Youku(YoukuBase):
                                          'loader\.swf\?VideoIDS=([a-zA-Z0-9=]+)',\
                                          'player\.youku\.com/embed/([a-zA-Z0-9=]+)')
 
-        api_url = 'http://play.youku.com/play/get.json?vid=%s&ct=12' % self.vid
-        api_url1 = 'http://play.youku.com/play/get.json?vid=%s&ct=10' % self.vid
-        try:
-            meta = json.loads(get_content(api_url))
-            meta1 = json.loads(get_content(api_url1))
-            data = meta['data']
-            data1 = meta1['data']
-            assert 'stream' in data1
-        except:
-            if 'error' in data1:
-                if data1['error']['code'] == -202:
-                    # Password protected
-                    self.password_protected = True
-                    self.password = input(log.sprint('Password: ', log.YELLOW))
-                    api_url += '&pwd={}'.format(self.password)
-                    api_url1 += '&pwd={}'.format(self.password)
-                    meta1 = json.loads(get_content(api_url1))
-                    meta = json.loads(get_content(api_url))
-                    data1 = meta1['data']
-                    data = meta['data']
-                else:
-                    raise AssertionError('[Failed] ' + data1['error']['note'])
-            else:
-                raise AssertionError('[Failed] Video not found.')
+        api_url = 'https://ups.youku.com/ups/get.json?vid={}&ccode=0401&client_ip=192.168.1.1&utid=&client_ts={}'.format(self.vid, int(time.time()))
 
+        data = json.loads(get_content(api_url))
+        assert data['e']['code'] == 0, data['e']['desc']
+        data = data['data']
         info.title = data['video']['title']
-        self.ep = data['security']['encrypt_string']
-        self.ip = data['security']['ip']
-        try:
-            self.stream_data = data1['stream']
-        except:
-            if self.password_protected:
-                raise AssertionError('incorrect password!!')
-            else:
-                raise AssertionError('No Stream found!!')
+        streams = data['stream']
+        for s in streams:
+            t = stream_code_to_id[s['stream_type']]
+            urls = [ json.loads(get_content(u['cdn_url']+'&yxon=1&special=true'))[0]['server']  for   u in s['segs']]
+            size = s['size']
+            info.stream_types.append(t)
+            info.streams[t] =  {
+                    'container': id_to_container[t],
+                    'video_profile': stream_code_to_profiles[t],
+                    'size': size,
+                    'src' : urls
+                }
+        info.stream_types = sorted(info.stream_types, key = ids.index)
+        return info
+
 
 site = Youku()

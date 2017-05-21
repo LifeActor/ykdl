8a6e40f67162c762c3906b5c541e32b97cbddf01
Zhang Ning <zhangn1985@gmail.com>
2017-05-21 11:16:16 +0800

    update common
    1, change the way get domain
    2, add debug trace in url_to_domain
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/common.py
+++ ykdl/common.py
@@ -18,34 +18,40 @@ alias = {
         'douyutv' : 'douyu',
         'aixifan' : 'acfun'
 }
+exclude_list = ['com', 'net', 'org']
 def url_to_module(url):
-    video_host = match1(url, 'https?://([^/]+)/')
-    video_url = match1(url, 'https?://[^/]+(.*)')
-    assert video_host and video_url, 'invalid url: ' + url
-
-    if video_host.endswith('.com.cn'):
-        video_host = video_host[:-3]
-    domain = match1(video_host, '(\.[^.]+\.[^.]+)$') or video_host
-    assert domain, 'unsupported url: ' + url
-    k = match1(domain, '([^.]+)')
-    if k in alias.keys():
-        k = alias[k]
+    if not url.startswith("http"):
+        logger.warning("> url not starts with http(s) " + url)
+        logger.warning("> assume http connection!")
+        url = "http://" + url
+    video_host = url.split('/')[2]
+    host_list = video_host.split('.')
+    if host_list[-2] in exclude_list:
+        short_name = host_list[-3]
+    else:
+        short_name = host_list[-2]
+    logger.debug('video_host> ' + video_host)
+    logger.debug('short_name> ' + short_name)
+    if short_name in alias.keys():
+        short_name = alias[short_name]
     try:
-        m = import_module('.'.join(['ykdl','extractors', k]))
+        m = import_module('.'.join(['ykdl','extractors', short_name]))
         if hasattr(m, "get_extractor"):
             site = m.get_extractor(url)
         else:
             site = m.site
         return site, url
     except(ImportError):
+        logger.debug('> Try HTTP Redirection!')
         from ykdl.compact import HTTPConnection
         conn = HTTPConnection(video_host)
-        conn.request("HEAD", video_url, headers=fake_headers)
+        conn.request("HEAD", url, headers=fake_headers)
         res = conn.getresponse()
         location = res.getheader('location')
         if location is None:
+            logger.debug('> NO HTTP Redirection')
+            logger.debug('> Go Generalembed')
             return import_module('ykdl.extractors.generalembed').site, url
-        elif location != url:
-            return url_to_module(location)
         else:
-            raise ConnectionResetError(url)
+            logger.debug('New Location> ' + location)
+            return url_to_module(location)

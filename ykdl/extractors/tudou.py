04c7797b298c5c11d7c8283c9d2e46b79d3d7ead
Zhang Ning <zhangn1985@gmail.com>
2017-05-25 21:26:03 +0800

    Tudou: handle old tudou url
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/tudou.py
+++ ykdl/extractors/tudou.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from .youku import Youku
+from ykdl.util.html import get_location
+import re
 
 class Tudou(Youku):
     name = u"Tudou (土豆)"
@@ -9,5 +11,10 @@ class Tudou(Youku):
     def __init__(self):
         Youku.__init__(self)
         self.ccode = '0402'
+
+    def prepare(self):
+        if not re.search('video.tuodou.com', self.url):
+            self.url = get_location(self.url)
+        return Youku.prepare(self)
 
 site = Tudou()

c09fdd6087a580cda6b07eaa3f2967419c65f218
Zhang Ning <zhangn1985@gmail.com>
2017-08-06 10:19:48 +0800

    support finance.le.com
    close #182
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/le/__init__.py
+++ ykdl/extractors/le/__init__.py
@@ -6,6 +6,8 @@ import re
 def get_extractor(url):
     if re.search("live.le", url):
         from . import live as s
+    elif re.search("finance.le", url):
+        from . import finance as s
     elif re.search("bcloud", url):
         from . import letvcloud as s
     else:

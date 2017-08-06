e7ec718638eea9d6f07a7239c5d51f21db79ee71
Zhang Ning <zhangn1985@gmail.com>
2017-08-06 12:55:09 +0800

    fix a mistake
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/qq/__init__.py
+++ ykdl/extractors/qq/__init__.py
@@ -6,7 +6,7 @@ import re
 def get_extractor(url):
     if re.search("live.qq", url):
         from . import live as s
-    if re.search("egame.qq", url):
+    elif re.search("egame.qq", url):
         from . import egame as s
     else:
         from . import video as s

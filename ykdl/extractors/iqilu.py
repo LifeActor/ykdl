b39d5c9811d32b5c7abc886d10205bb440460f1d
Zhang Ning <zhangn1985@gmail.com>
2017-08-06 09:59:32 +0800

    update iqilu
    from
    https://github.com/soimort/you-get/commit/6dd51760c1497155414fc40d427202a6e5c735bd
    
    close #179
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/iqilu.py
+++ ykdl/extractors/iqilu.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ..simpleextractor import SimpleExtractor
+from ykdl.simpleextractor import SimpleExtractor
+from ykdl.util.match import match1
+
 import re
+import json
 
 class Iqilu(SimpleExtractor):
     name = u"齐鲁网 (iqilu)"
@@ -10,9 +13,12 @@ class Iqilu(SimpleExtractor):
     def __init__(self):
         SimpleExtractor.__init__(self)
         self.title_pattern = '<meta name="description" content="(.*?)\"\W'
-        self.url_pattern = "<input type='hidden' id='playerId' url='(.+)'"
 
     def l_assert(self):
         assert re.match(r'http://v.iqilu.com/\w+', self.url)
+
+    def get_url(self):
+        player_data =  '[' + match1(self.html, 'url\s*:\s*\[([^\]]+)\]') + ']'
+        self.v_url = [json.loads(player_data)[0]['stream_url']]
 
 site = Iqilu()

21f5bc49f523b5ce6e166cdb30e80bceb116fe06
Zhang Ning <zhangn1985@gmail.com>
2017-09-09 13:25:05 +0800

    support  vc.bilibili
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/bilibili/__init__.py
+++ ykdl/extractors/bilibili/__init__.py
@@ -6,6 +6,8 @@ import re
 def get_extractor(url):
     if re.search("live.bili", url):
         from . import live as s
+    elif re.search("vc.bili", url):
+        from . import vc as s
     else:
         from . import video as s
     return s.site

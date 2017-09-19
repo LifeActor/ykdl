af11517072ff24b05dcf1e4801e4a7d70ec03488
Zhang Ning <zhangn1985@gmail.com>
2017-09-20 07:09:02 +0800

    update iqilu
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/iqilu.py
+++ ykdl/extractors/iqilu.py
@@ -13,12 +13,10 @@ class Iqilu(SimpleExtractor):
     def __init__(self):
         SimpleExtractor.__init__(self)
         self.title_pattern = '<meta name="description" content="(.*?)\"\W'
+        self.url_pattern = 'id=\'playerId\' url=\'(.*)\''
 
     def l_assert(self):
         assert re.match(r'http://v.iqilu.com/\w+', self.url)
 
-    def get_url(self):
-        player_data =  '[' + match1(self.html, 'url\s*:\s*\[([^\]]+)\]') + ']'
-        self.v_url = [json.loads(player_data)[0]['stream_url']]
 
 site = Iqilu()

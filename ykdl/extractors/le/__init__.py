f4bb0fdb743f508d6536aeb44a9ac650ac8a3a68
Zhang Ning <zhangn1985@gmail.com>
2017-09-21 08:13:16 +0800

    support sports.le.com
    for #188
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/le/__init__.py
+++ ykdl/extractors/le/__init__.py
@@ -6,7 +6,7 @@ import re
 def get_extractor(url):
     if re.search("lunbo", url):
         from . import lunbo as s
-    elif re.search("finance", url):
+    elif re.search("finance", url) or re.search("sports", url):
         from . import finance as s
     elif re.search("bcloud", url):
         from . import letvcloud as s
--- ykdl/extractors/le/finance.py
+++ ykdl/extractors/le/finance.py
@@ -27,7 +27,7 @@ class LeLiveFi(VideoExtractor):
     def prepare(self):
         info = VideoInfo(self.name, True)
         html = get_content(self.url)
-        self.vid = match1(html, 'liveId\s*:\s*"(\d+)"') or match1(self.url, 'd=(\d+)')
+        self.vid = match1(html, 'liveId\s*:\s*"(\d+)"') or match1(self.url, 'd=(\d+)', 'live/(\d+)')
 
         live_data = json.loads(get_content('http://player.pc.le.com/player/startup_by_pid/1001/{}?host=live.le.com'.format(self.vid)))
         assert live_data['status'] == 2, "Live show is finished, playback is not supported!"

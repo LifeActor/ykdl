db10e502339ae30940d1fb016605baa5025637bf
Zhang Ning <zhangn1985@gmail.com>
2017-09-21 08:17:03 +0800

    better error handling
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/extractors/le/finance.py
+++ ykdl/extractors/le/finance.py
@@ -30,7 +30,7 @@ class LeLiveFi(VideoExtractor):
         self.vid = match1(html, 'liveId\s*:\s*"(\d+)"') or match1(self.url, 'd=(\d+)', 'live/(\d+)')
 
         live_data = json.loads(get_content('http://player.pc.le.com/player/startup_by_pid/1001/{}?host=live.le.com'.format(self.vid)))
-        assert live_data['status'] == 2, "Live show is finished, playback is not supported!"
+        assert 'status' in live_data and live_data['status'] == 2, "Live show is finished, playback is not supported!"
         info.title = live_data['title']
 
         stream_data = live_data['rows']

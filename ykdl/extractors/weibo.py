86a606dafeeea24139b61c0ca1066fa88fc5ef29
SeaHOH <Seahoh@Gmail.com>
2017-10-04 15:52:22 +0800

    fix weibo

--- ykdl/extractors/weibo.py
+++ ykdl/extractors/weibo.py
@@ -10,7 +10,7 @@ class Weibo(SimpleExtractor):
     def __init__(self):
         SimpleExtractor.__init__(self)
         add_header('User-Agent', 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36')
-        self.url_pattern = 'data-url=\"([^\"]+)'
+        self.url_pattern = '(?:data-url|video_src|controls src)=(?:\"|\')([^\"\']+)'
         self.title_pattern = '<title>([^<]+)</'
 
     def l_assert(self):

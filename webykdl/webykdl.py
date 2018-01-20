90b85b4e37dbaeca24925ea8e0d94cdb67a28965
Zhang Ning <zhangn1985@gmail.com>
2018-01-20 21:18:05 +0800

    webykdl: support playlist
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- webykdl/webykdl.py
+++ webykdl/webykdl.py
@@ -15,7 +15,7 @@ except:
     player.start()
 
 import json
-
+import types
 
 from ykdl.common import url_to_module
 
@@ -23,16 +23,33 @@ from ykdl.common import url_to_module
 def play():
     if request.method == 'POST':
         url = request.form['url']
-        m,u = url_to_module(url)
         try:
-            info = m.parser(u)
-        except AssertionError as e: 
-            return str(e)
-        player_args = info.extra
-        player_args['title'] = info.title
-        urls = info.streams[info.stream_types[0]]['src']
-        video = json.dumps({"urls": urls, "args": player_args})
-        player.play(video)
+            islist = request.form['list']
+            islist = islist == "True"
+        except:
+            islist = False
+        m,u = url_to_module(url)
+        if not islist:
+            parser = m.parser
+        else:
+            parser = m.parser_list
+        try:
+           info = parser(u)
+        except AssertionError as e:
+           return str(e)
+        if type(info) is types.GeneratorType or type(info) is list:
+            for i in info:
+                player_args = i.extra
+                player_args['title'] = i.title
+                urls = i.streams[i.stream_types[0]]['src']
+                video = json.dumps({"urls": urls, "args": player_args})
+                player.play(video)
+        else:
+            player_args = info.extra
+            player_args['title'] = info.title
+            urls = info.streams[info.stream_types[0]]['src']
+            video = json.dumps({"urls": urls, "args": player_args})
+            player.play(video)
         return "OK"
     else:
         return "curl --data-urlencode \"url=<URL>\" http://IP:5000/play"
@@ -55,6 +72,7 @@ def index():
 <style>form{float:left;}</style>
 <form action="/play" method="post" target="_blank">
   输入视频网址: <input type="text" name="url" />
+播放列表？<input type="checkbox" name="list" value="True" />
   <input type="submit" value="播放" />
 </form>
 <form action="/stop" method="get" target="_blank">

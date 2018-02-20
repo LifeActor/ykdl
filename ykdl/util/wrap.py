96f939f1e39874af41a0cfab73ba7e25389863f7
Zhang Ning <zhangn1985@gmail.com>
2018-02-20 16:15:36 +0800

    mpv: change default mpv parameters
    close: #223
    
    Signed-off-by: Zhang Ning <zhangn1985@gmail.com>

--- ykdl/util/wrap.py
+++ ykdl/util/wrap.py
@@ -22,9 +22,7 @@ def launch_player(player, urls, **args):
     else:
         cmd = [player]
     if 'mpv' in cmd[0]:
-        cmd += ['--no-ytdl', '--demuxer-lavf-o', 'protocol_whitelist=[file,tcp,http]']
-        if len(urls) > 1:
-            cmd += ['--merge-files']
+        cmd += ['--demuxer-lavf-o', 'protocol_whitelist=[file,tcp,http]']
         if args['ua']:
             cmd += ['--user-agent', args['ua']]
         if args['referer']:

27eab3442fb61d6ee5de05edb45c2c7defe92382
SeaHOH <Seahoh@Gmail.com>
2018-03-26 22:41:32 +0800

    Add files via upload

--- cykdl/__main__.py
+++ cykdl/__main__.py
@@ -114,7 +114,9 @@ def handle_videoinfo(info, index=0):
     if info.extra['rangefetch']:
         info.extra['rangefetch']['down_rate'] = info.extra['rangefetch']['video_rate'][stream_id]
     if args.proxy != 'none':
-        info.extra['proxy'] = 'http://' + args.proxy
+        proxy = 'http://' + args.proxy
+        info.extra['proxy'] = proxy
+        info.extra['rangefetch']['proxy'] = proxy
     player_args = info.extra
     player_args['title'] = info.title
     if args.player:

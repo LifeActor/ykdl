633175dd3df6f5a4e8ad1f15580b447cf0258dcf
SeaHOH <Seahoh@Gmail.com>
2018-05-27 21:18:16 +0800

    netease: fix openc

--- ykdl/extractors/netease/openc.py
+++ ykdl/extractors/netease/openc.py
@@ -3,17 +3,14 @@
 
 from ykdl.simpleextractor import SimpleExtractor
 from ykdl.util.match import match1
+from ykdl.util.html import get_content
 
 class OpenC(SimpleExtractor):
     name = u"网易公开课 (163 openCourse)"
 
     def __init__(self):
         SimpleExtractor.__init__(self)
-        self.url_pattern = 'appsrc : ([^ ]+)'
-        self.title_pattern = 'title : ([^ ]+)'
-
-    def get_url(self):
-        url = match1(self.html, self.url_pattern)
-        self.v_url = [url[2:-5]]
+        self.url_pattern = 'appsrc : .(.+?).,\n'
+        self.title_pattern = 'title : .(.+?).,\n'
 
 site = OpenC()

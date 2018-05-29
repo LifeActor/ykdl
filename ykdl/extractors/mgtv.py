96b6e9fe743c1f09469fc7afa2cd61dde859eb38
SeaHOH <Seahoh@Gmail.com>
2018-05-30 00:54:07 +0800

    mgtv: update
    close #281

--- ykdl/extractors/mgtv.py
+++ ykdl/extractors/mgtv.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from ykdl.util.html import get_content
+from ykdl.util.html import default_proxy_handler, get_content
 from ykdl.util.match import match1, matchall
 from ykdl.extractor import VideoExtractor
 from ykdl.videoinfo import VideoInfo
+from ykdl.compact import install_opener, build_opener, HTTPCookieProcessor
 
 import json
+import sys
+import base64
+import uuid
+import time
+
+py3 = sys.version_info[0] == 3
+if py3:
+    maketrans = bytes.maketrans
+    bytearray2str = bytearray.decode
+else:
+    from string import maketrans 
+    bytearray2str = str
+
+encode_translation = maketrans(b'+/=', b'_~-')
+
+def generate_did_tk2():
+    did = str(uuid.uuid4())
+    s = 'pno=1000|ver=0.3.0001|did={}|clit={}'.format(did, int(time.time()))
+    if not isinstance(s, bytes):
+        s = s.encode()
+    e = bytearray(base64.b64encode(s).translate(encode_translation))
+    e.reverse()
+    return did, bytearray2str(e)
 
 class Hunantv(VideoExtractor):
     name = u"芒果TV (HunanTV)"
@@ -16,22 +40,35 @@ class Hunantv(VideoExtractor):
     profile_2_types = { u'蓝光': 'BD', u'超清': 'TD', u'高清': 'HD', u'标清': 'SD' }
     
     def prepare(self):
+        handlers = [HTTPCookieProcessor()]
+        if default_proxy_handler:
+            handlers += default_proxy_handler
+        install_opener(build_opener(*handlers))
+
         info = VideoInfo(self.name)
         if self.url and not self.vid:
-            self.vid = match1(self.url, 'http://www.mgtv.com/b/\d+/(\d+).html')
+            self.vid = match1(self.url, 'https?://www.mgtv.com/b/\d+/(\d+).html')
             if self.vid is None:
                 html = get_content(self.url)
                 self.vid = match1(html, 'vid=(\d+)', 'vid=\"(\d+)', 'vid: (\d+)')
 
-        api_url = 'http://pcweb.api.mgtv.com/player/video?video_id={}'.format(self.vid)
-        meta = json.loads(get_content(api_url))
+        did, tk2 = generate_did_tk2()
+        api_info_url = 'https://pcweb.api.mgtv.com/player/video?video_id={}&did={}&tk2={}'.format(self.vid, did, tk2)
+        meta = json.loads(get_content(api_info_url))
 
-        assert meta['code'] == 200, '[failed] status: {}, msg: {}'.format(meta['status'],meta['msg'])
-        assert meta['data'], '[Failed] Video not found.'
+        assert meta['code'] == 200, '[failed] code: {}, msg: {}'.format(meta['code'], meta['msg'])
+        assert meta['data'], '[Failed] Video info not found.'
+
+        pm2 = meta['data']['atc']['pm2']
+        info.title = meta['data']['info']['title']
+
+        api_source_url = 'https://pcweb.api.mgtv.com/player/getSource?video_id={}&did={}&pm2={}&tk2={}'.format(self.vid, did, pm2, tk2)
+        meta = json.loads(get_content(api_source_url))
+
+        assert meta['code'] == 200, '[failed] code: {}, msg: {}'.format(meta['code'], meta['msg'])
+        assert meta['data'], '[Failed] Video source not found.'
 
         data = meta['data']
-
-        info.title = data['info']['title']
         domain = data['stream_domain'][0]
         for lstream in data['stream']:
             if lstream['url']:

26637df552b3a71b973cfc4b44c94a2de78e1c13
SeaHOH <Seahoh@Gmail.com>
2018-10-07 15:04:20 +0800

    netease: add 3g.163

--- ykdl/extractors/netease/__init__.py
+++ ykdl/extractors/netease/__init__.py
@@ -11,6 +11,8 @@ def get_extractor(url):
     elif re.search("music.163", url):
         from . import music as s
         return s.get_extractor(url)
+    elif re.search("3g.163", url):
+        from . import m3g as s
     else:
         from . import video as s
     return s.site

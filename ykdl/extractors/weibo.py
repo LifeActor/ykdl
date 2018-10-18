c5afd620d5db5a111075d92eca995943eb760ef9
SeaHOH <Seahoh@Gmail.com>
2018-10-18 23:13:27 +0800

    Weibo: add mobile html parse

--- ykdl/extractors/weibo.py
+++ ykdl/extractors/weibo.py
@@ -11,10 +11,29 @@ class Weibo(SimpleExtractor):
     def __init__(self):
         SimpleExtractor.__init__(self)
         add_header('User-Agent', 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36')
-        self.url_pattern = '(?:data-url|video_src|controls src)=(?:\"|\')([^\"\']+)'
-        self.title_pattern = '<title>([^<]+)</'
+
+    def get_title(self):
+        if self.title_patterns:
+            self.info.title = match1(self.html, *self.title_patterns)
+        # JSON string escaping in safe
+        exec('self.info.title = """%s"""' % self.info.title.replace('"""', ''))
+
+    def get_url(self):
+        if self.url_patterns:
+            self.v_url = [match1(self.html, *self.url_patterns)]
 
     def l_assert(self):
+        # Mobile ver.
+        self.title_patterns = '"content2": "(.+?)",', '"status_title": "(.+?)",'
+        self.url_patterns = '"stream_url_hd": "([^"]+)', '"stream_url": "([^"]+)'
+        if 'm.weibo.cn' in self.url:
+            return
+        if 'weibo.com' in self.url and '/tv/v/' not in self.url and 'fid=' not in self.url:
+            self.url = self.url.replace('weibo.com', 'm.weibo.cn', 1)
+            return
+
+        self.url_patterns = r'(?:data-url|controls src)\s*=\s*[\"\']([^\"\']+)',
+        self.title_patterns = '<title>([^<]+)</',
         self.url = self.url.replace('%3A', ':')
         fid = match1(self.url, r'\?fid=(\d{4}:\w+)')
         if fid is not None:
@@ -24,12 +43,9 @@ class Weibo(SimpleExtractor):
             self.url = 'http://video.weibo.com/show?fid=1034:{}&type=mp4'.format(fid)
         else:
             html = get_content(self.url)
-            url = match1(html, r'"page_url"\s*:\s*"([^"]+)"')
+            url = match1(html, '"page_url": "([^"]+)')
             assert url, 'No url match'
             self.url = url
             self.l_assert()
-
-    def get_title(self):
-        self.title = SimpleExtractor.get_title(self) or self.name
 
 site = Weibo()

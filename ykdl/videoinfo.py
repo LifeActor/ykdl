e13b8def4e4a3779e9f38334fd2fabd95ad31213
SeaHOH <Seahoh@Gmail.com>
2018-12-13 12:21:06 +0800

    fix index of size key

--- ykdl/videoinfo.py
+++ ykdl/videoinfo.py
@@ -8,6 +8,7 @@ import datetime
 import random
 from ykdl.util.fs import legitimize
 from ykdl.util import log
+from ykdl.util.wrap import encode_for_wrap
 
 class VideoInfo():
     def __init__(self, site, live = False):
@@ -45,7 +46,7 @@ class VideoInfo():
         json_dict['stream_types'] = self.stream_types
         json_dict['extra'] = self.extra
         for s in json_dict['streams']:
-            if json_dict['streams'][s]['size'] == float('inf'):
+            if json_dict['streams'][s].get('size') == float('inf'):
                 json_dict['streams'][s].pop('size')
         return json_dict
 
@@ -69,4 +70,4 @@ class VideoInfo():
             name_list.append(stream_id)
         if self.live:
             name_list.append(datetime.datetime.now().isoformat())
-        return legitimize('_'.join(name_list))
+        return encode_for_wrap(legitimize('_'.join(name_list)), 'ignore')

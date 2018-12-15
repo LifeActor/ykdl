afa1fcf8802070b0830ae42ce7a15d88e1d1b0b4
SeaHOH <Seahoh@Gmail.com>
2018-12-15 18:52:33 +0800

    le: add live playback
    close #188

--- ykdl/extractors/le/__init__.py
+++ ykdl/extractors/le/__init__.py
@@ -6,8 +6,8 @@ import re
 def get_extractor(url):
     if re.search("lunbo", url):
         from . import lunbo as s
-    elif re.search("finance", url) or re.search("sports", url):
-        from . import finance as s
+    elif re.search("(live[\./]|/izt/)", url):
+        from . import live as s
     elif re.search("bcloud", url):
         from . import letvcloud as s
     else:

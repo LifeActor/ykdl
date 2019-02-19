071a8cf255b9b52b2c6e6bccd7d2d0591977ab1c
coslyk <cos.lyk@gmail.com>
2019-02-19 13:42:06 +0100

    Add win support & improve interpreter detection

--- ykdl/util/jsengine.py
+++ ykdl/util/jsengine.py
@@ -5,9 +5,12 @@
     
     Description:
         This library wraps the system's built-in Javascript interpreter to python.
-        macOS: Use JavascriptCore
-        Linux: Use gjs on Gnome, cjs on Cinnamon or NodeJS if installed
-        Windows: Currently not implemented
+
+    Platform:
+        macOS:   Use JavascriptCore
+        Linux:   Use gjs on Gnome, cjs on Cinnamon or NodeJS if installed
+        Windows: Use NodeJS if available, otherwise use Windows Script Host
+                 Note that NodeJS is recommended, because WSH does not support ArrayBuffer
     
     Usage:
         ctx = JSEngine()
@@ -21,13 +24,15 @@
         ctx2.call("add", 1, 2)  # => 3
 '''
 
+from __future__ import print_function
+from distutils.spawn import find_executable
 from subprocess import Popen, PIPE
-from os.path import isfile
 import io
 import json
 import os
 import platform
 import re
+import sys
 import tempfile
 
 # Exceptions
@@ -42,48 +47,97 @@ class RuntimeError(Exception):
 # macOS: use built-in JavaScriptCore
 if platform.system() == 'Darwin':
     interpreter = ['/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc']
+# Windows: prefer Node.js, if not found use Windows Script Host
+elif platform.system() == 'Windows':
+    if find_executable('node') is not None:
+        interpreter = ['node']
+    else:
+        print('It is recommended to install NodeJS, because the built-in Javascript interpreter of Windows does not support ArrayBuffer', file=sys.stderr)
+        interpreter = ['cscript', '//E:jscript', '//Nologo']
 # Linux: use gjs on Gnome, cjs on Cinnamon or NodeJS if installed
 elif platform.system() == 'Linux':
-    if isfile('/usr/bin/gjs') or isfile('/usr/local/bin/gjs' or isfile('/app/bin/gjs')):
+    if find_executable('gjs') is not None:
         interpreter = ['gjs']
-    elif isfile('/usr/bin/cjs') or isfile('/usr/local/bin/cjs' or isfile('/app/bin/cjs')):
+    elif find_executable('cjs') is not None:
         interpreter = ['cjs']
-    elif isfile('/usr/bin/nodejs') or isfile('/usr/local/bin/nodejs' or isfile('/app/bin/nodejs')):
+    elif find_executable('nodejs') is not None:
         interpreter = ['nodejs']
+    elif find_executable('node') is not None:
+        interpreter = ['node']
     else:
         raise RuntimeError('Please install at lease one of the following Javascript interpreter: gjs, cjs, nodejs')
 else:
     raise RuntimeError('Sorry, the Javascript engine is currently not supported on your system')
 
 
+
 # Inject to the script to let it return jsonlized value to python
-injected_script = r'''
-(function(program, execJS) { execJS(program) })(
-function() {
-    return eval(#{encoded_source});
-},
-function(program) {
-    var output;
-    try {
-        result = program();
-        print("");
-        if (typeof result == 'undefined' && result !== null) {
-            print('["ok", 0]');
-        }
-        else {
-            try {
-                print(JSON.stringify(['ok', result]));
+if interpreter[0] == 'cscript':
+    injected_script = r'''
+    (function(program, execJS) { execJS(program) })(
+    function() {
+        return eval(#{encoded_source});
+    },
+    function(program) {
+        #{json2_source}
+        var output, print = function(string) {
+            string = string.replace(/[^\x00-\x7f]/g, function(ch){
+                return '\\u' + ('0000' + ch.charCodeAt(0).toString(16)).slice(-4);
+            });
+            WScript.Echo(string);
+        };
+        try {
+            result = program();
+            print("")
+            if (typeof result == 'undefined' && result !== null) {
+                print('["ok"]');
             }
-            catch (err) {
-                print('["err", 0]');
+            else {
+                try {
+                    print(JSON.stringify(['ok', result]));
+                }
+                catch (err) {
+                    print('["err", "Script returns a value with an unknown type"]');
+                }
             }
         }
-    }
-    catch (err) {
-        print(JSON.stringify(['err', '' + err]));
-    }
-});
-'''
+        catch (err) {
+            print(JSON.stringify(['err', err.name + ': ' + err.message]));
+        }
+    });
+    '''
+    json2_source = 'var JSON;if(!JSON){JSON={}}(function(){function f(n){return n<10?"0"+n:n}if(typeof Date.prototype.toJSON!=="function"){Date.prototype.toJSON=function(key){return isFinite(this.valueOf())?this.getUTCFullYear()+"-"+f(this.getUTCMonth()+1)+"-"+f(this.getUTCDate())+"T"+f(this.getUTCHours())+":"+f(this.getUTCMinutes())+":"+f(this.getUTCSeconds())+"Z":null};String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(key){return this.valueOf()}}var cx=/[\\u0000\\u00ad\\u0600-\\u0604\\u070f\\u17b4\\u17b5\\u200c-\\u200f\\u2028-\\u202f\\u2060-\\u206f\\ufeff\\ufff0-\\uffff]/g,escapable=/[\\\\\\"\\x00-\\x1f\\x7f-\\x9f\\u00ad\\u0600-\\u0604\\u070f\\u17b4\\u17b5\\u200c-\\u200f\\u2028-\\u202f\\u2060-\\u206f\\ufeff\\ufff0-\\uffff]/g,gap,indent,meta={"\\b":"\\\\b","\\t":"\\\\t","\\n":"\\\\n","\\f":"\\\\f","\\r":"\\\\r",\'"\':\'\\\\"\',"\\\\":"\\\\\\\\"},rep;function quote(string){escapable.lastIndex=0;return escapable.test(string)?\'"\'+string.replace(escapable,function(a){var c=meta[a];return typeof c==="string"?c:"\\\\u"+("0000"+a.charCodeAt(0).toString(16)).slice(-4)})+\'"\':\'"\'+string+\'"\'}function str(key,holder){var i,k,v,length,mind=gap,partial,value=holder[key];if(value&&typeof value==="object"&&typeof value.toJSON==="function"){value=value.toJSON(key)}if(typeof rep==="function"){value=rep.call(holder,key,value)}switch(typeof value){case"string":return quote(value);case"number":return isFinite(value)?String(value):"null";case"boolean":case"null":return String(value);case"object":if(!value){return"null"}gap+=indent;partial=[];if(Object.prototype.toString.apply(value)==="[object Array]"){length=value.length;for(i=0;i<length;i+=1){partial[i]=str(i,value)||"null"}v=partial.length===0?"[]":gap?"[\\n"+gap+partial.join(",\\n"+gap)+"\\n"+mind+"]":"["+partial.join(",")+"]";gap=mind;return v}if(rep&&typeof rep==="object"){length=rep.length;for(i=0;i<length;i+=1){if(typeof rep[i]==="string"){k=rep[i];v=str(k,value);if(v){partial.push(quote(k)+(gap?": ":":")+v)}}}}else{for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=str(k,value);if(v){partial.push(quote(k)+(gap?": ":":")+v)}}}}v=partial.length===0?"{}":gap?"{\\n"+gap+partial.join(",\\n"+gap)+"\\n"+mind+"}":"{"+partial.join(",")+"}";gap=mind;return v}}if(typeof JSON.stringify!=="function"){JSON.stringify=function(value,replacer,space){var i;gap="";indent="";if(typeof space==="number"){for(i=0;i<space;i+=1){indent+=" "}}else{if(typeof space==="string"){indent=space}}rep=replacer;if(replacer&&typeof replacer!=="function"&&(typeof replacer!=="object"||typeof replacer.length!=="number")){throw new Error("JSON.stringify")}return str("",{"":value})}}if(typeof JSON.parse!=="function"){JSON.parse=function(text,reviver){var j;function walk(holder,key){var k,v,value=holder[key];if(value&&typeof value==="object"){for(k in value){if(Object.prototype.hasOwnProperty.call(value,k)){v=walk(value,k);if(v!==undefined){value[k]=v}else{delete value[k]}}}}return reviver.call(holder,key,value)}text=String(text);cx.lastIndex=0;if(cx.test(text)){text=text.replace(cx,function(a){return"\\\\u"+("0000"+a.charCodeAt(0).toString(16)).slice(-4)})}if(/^[\\],:{}\\s]*$/.test(text.replace(/\\\\(?:["\\\\\\/bfnrt]|u[0-9a-fA-F]{4})/g,"@").replace(/"[^"\\\\\\n\\r]*"|true|false|null|-?\\d+(?:\\.\\d*)?(?:[eE][+\\-]?\\d+)?/g,"]").replace(/(?:^|:|,)(?:\\s*\\[)+/g,""))){j=eval("("+text+")");return typeof reviver==="function"?walk({"":j},""):j}throw new SyntaxError("JSON.parse")}}}());'
+    injected_script = injected_script.replace('#{json2_source}', json2_source)
+else:
+    injected_script = r'''
+    (function(program, execJS) { execJS(program) })(
+    function() {
+        return eval(#{encoded_source});
+    },
+    function(program) {
+        var output;
+        try {
+            result = program();
+            print("");
+            if (typeof result == 'undefined' && result !== null) {
+                print('["ok"]');
+            }
+            else {
+                try {
+                    print(JSON.stringify(['ok', result]));
+                }
+                catch (err) {
+                    print('["err", "Script returns a value with an unknown type"]');
+                }
+            }
+        }
+        catch (err) {
+            print(JSON.stringify(['err', '' + err]));
+        }
+    });
+    '''
+
+
 
 class JSEngine:
     def __init__(self, source = None):
@@ -109,6 +163,8 @@ class JSEngine:
         output = output.replace('\r\n', '\n').replace('\r', '\n')
         last_line = output.split('\n')[-2]
         ret = json.loads(last_line)
+        if len(ret) == 1:
+            return None
         status, value = ret
         if status == 'ok':
             return value
@@ -135,6 +191,8 @@ class JSEngine:
                 ret = p.wait()
             finally:
                 del p
+            if ret != 0:
+                raise RuntimeError('Javascript interpreter returns non-zero value!')
             return stdoutdata
         finally:
             os.remove(filename)

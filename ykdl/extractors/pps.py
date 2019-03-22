412f4054341735c0aeeb76dba6e0537231219478
SeaHOH <Seahoh@Gmail.com>
2019-03-22 11:46:44 +0800

    pps (iqiyi.qixiu): change API
    #393

--- ykdl/extractors/pps.py
+++ ykdl/extractors/pps.py
@@ -1,64 +1,118 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
-
-from ykdl.util.match import match1
-from ykdl.util.html import get_content, add_header
 from ykdl.extractor import VideoExtractor
 from ykdl.videoinfo import VideoInfo
+from ykdl.util.html import get_content, add_header
+from ykdl.util.match import match1
 from ykdl.compact import urlencode
+
+from .iqiyi.util import get_macid
+
+import json
 import time
+import random
+import hashlib
 
-class PPS(VideoExtractor):
 
+def gsign(params):
+    s = []
+    for key in sorted(params.keys()):
+        s.append('{}:{}'.format(key, params[key]))
+    s.append('w!ytDgy#lEXWoJmN4HPf')
+    s = ''.join(s)
+    return hashlib.sha1(s.encode('utf8')).hexdigest()
+
+def getlive(uid, rate='source'):
+    tm = int(time.time())
+    api = 'https://m-glider-xiu.pps.tv/v2/stream/get.json'
+    params = {
+        'type_id': 1,
+        'vid': 1,
+        'anchor_id': uid,
+        'app_key': 'show_web_h5',
+        'version': '1.0.0',
+        'platform': '1_10_101',
+        'time': tm,
+        'netstat': 'wifi',
+        'device_id': get_macid(),
+        'bit_rate_type': rate,
+        'protocol': 5,
+    }
+    params['sign'] = gsign(params)
+    data = urlencode(params)
+    if not isinstance(data, bytes):
+        data = data.encode()
+    html = get_content(api, data=data)
+    return json.loads(html)
+
+class IqiyiLive(VideoExtractor):
     name = u"奇秀（Qixiu)"
 
+    ids = ['TD', 'HD', 'SD']
+    rate_2_id = {
+        'source': 'TD',
+        'high': 'HD',
+        'smooth': 'SD'
+    }
+    rate_2_profile = {
+        'source': u'超清',
+        'high': u'高清',
+        'smooth': u'标清'
+    }
+
     def prepare(self):
+        info = VideoInfo(self.name, True)
+        html = get_content(self.url)
+        self.vid = match1(html, '"user_id":"([^"]+)",')
+        title = json.loads(match1(html, '"room_name":("[^"]+"),'))
+        artist = json.loads(match1(html, '"nick_name":("[^"]+"),'))
+        info.title = u'{} - {}'.format(title, artist)
+        info.artist = artist
 
-        info = VideoInfo(self.name)
-        if self.url:
-            self.vid = match1(self.url, r'/room/(\d+)')
-            
-        # Get title and anchor id
-        url = 'https://m-x.pps.tv/room/' + self.vid
-        ua = 'Mozilla/5.0 (iPad; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1'
-        add_header('Referer', url)
-        add_header('User-Agent', ua)
-        info.extra['ua'] = ua
-        info.extra['referer'] = url
-        html = get_content(url)
-        anchor_id = match1(html, r'"anchor_id":"(.+?)"')
-        info.title = match1(html, r'"nick_name":"(.+?)"')
+        def get_live_info(rate='source'):
+            data = getlive(self.vid, rate)
+            self.logger.debug('data:\n' + str(data))
+            if data['code'] != 'A00000':
+                return data.get('msg', '')
 
-        # Get IP from iqiyi's server
-        tm = int(time.time())
-        api_url = 'http://data.video.qiyi.com/v.mp4?_={}&callback=jsonCallBack'.format(tm)
-        html = get_content(api_url)
-        ip = match1(html, r'-(\d+\.\d+\.\d+\.\d+)')
+            data = data['data']
+            url = data.get('https_flv', data.get('flv', data.get('rtmp')))
+            if url:
+                url = url.replace('rtmp://', 'http://')
+                ran = random.randrange(1e4)
+                if '?' in url:
+                    url = '{}&ran={}'.format(url, ran)
+                else:
+                    url = '{}?ran={}'.format(url, ran)
+                stream_profile = self.rate_2_profile[rate]
+                stream_id = self.rate_2_id[rate]
+                info.stream_types.append(stream_id)
+                info.streams[stream_id] = {
+                    'video_profile': stream_profile,
+                    'container': 'flv',
+                    'src' : [url],
+                    'size': float('inf')
+                }
 
-        # Get stream url
-        tm = int(time.time())
-        params = {
-            'qd_tm': tm,
-            'typeId': 1,
-            'platform': 7,
-            'vid': 0,
-            'qd_vip': 0,
-            'qd_uid': anchor_id,
-            'qd_ip': ip,
-            'qd_vipres': 0,
-            'qd_src': 'h5_xiu',
-            'qd_tvid': 0,
-            '_': tm,
-            'callback': 'jsonCallBack'
-        }
-        api_url = 'http://api-live.iqiyi.com/stream/geth5?' + urlencode(params)
-        html = get_content(api_url)
-        data = json.loads(html[13:-1])
-        url = data['data']['hls']
-        info.stream_types.append('current')
-        info.streams['current'] = {'video_profile': 'current', 'container': 'm3u8', 'src': [url], 'size' : 0}
+            error_msges = []
+            if rate == 'source':
+                rate_list = data['rate_list']
+                if 'source' in rate_list:
+                    rate_list.remove('source')
+                    for rate in rate_list:
+                        error_msg = get_live_info(rate)
+                        if error_msg:
+                            error_msges.append(error_msg)
+            if error_msges:
+                return ', '.join(error_msges)
+
+        error_msg = get_live_info()
+        if error_msg:
+            self.logger.debug('error_msg:\n' + error_msg)
+        assert len(info.stream_types), error_msg or 'can\'t play this live video!!'
+        info.stream_types = sorted(info.stream_types, key=self.ids.index)
+
         return info
 
-site = PPS()
+site = IqiyiLive()

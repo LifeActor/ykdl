dd4ceaa47cadeb6bef8637fc82ec8af8f151ff54
chigusa <c02110210610204@gmail.com>
2019-06-24 13:25:51 +0800

    fix egame

--- ykdl/extractors/qq/egame.py
+++ ykdl/extractors/qq/egame.py
@@ -9,38 +9,33 @@ from ykdl.compact import unescape
 import json
 import time
 
+
 class QQEGame(VideoExtractor):
     name = u'QQ EGAME (企鹅电竟)'
 
-    mutli_bitrate = ['0', '900', '550']
+    mutli_bitrate = ['0', '4000', '1500', '900']
 
-    bitrate_2_type = {'0': 'BD', '900': 'HD', '550': 'SD'}
+    bitrate_2_type = {'0': 'Souce', '4000': 'BD', '1500': 'HD', '900': 'SD'}
 
-    bitrate_2_profile = {'0': u'超清', '900': u'高清', '550': u'标清'}
+    bitrate_2_profile = {'0': u'原画', '4000': u'蓝光', '1500': u'高清', '900': u'流畅'}
 
     def prepare(self):
         info = VideoInfo(self.name, True)
+        html = get_content(self.url)
         if not self.vid:
             self.vid = match1(self.url, '/(\d+)')
         if not self.vid:
-            html = get_content(self.url)
             self.vid = match1(html, '"liveAddr":"([0-9\_]+)"')
         self.pid = self.vid
 
         # from upstream!!
-        serverDataTxt = match1(html, 'serverData = {([\S\ ]+)};')
-        serverDataTxt = '{%s}' % (serverDataTxt)
-        self.logger.debug("serverDataTxt => %s" % (serverDataTxt))
+        mobileHtml = get_content('https://m.egame.qq.com/live?anchorid={}'.format(self.vid))
+        assert match1(mobileHtml,'"isLive":(\d+)') == '1', 'error: live show is not on line!!'
 
-        serverData = json.loads(serverDataTxt)
-        self.logger.debug(serverData)
+        info.title = match1(html, 'title:"([^"]*)"')
+        info.artist = match1(html, 'nickName:"([^"]+)"')
 
-        assert serverData["liveInfo"]["data"]["profileInfo"]["isLive"] == 1, 'error: live show is not on line!!'
-
-        info.title = serverData["liveInfo"]["data"]["videoInfo"]["title"]
-        info.artist = serverData["liveInfo"]["data"]["profileInfo"]["nickName"]
-
-        for data in serverData["liveInfo"]["data"]["videoInfo"]["streamInfos"]:
+        for data in json.loads(match1(html, '"urlArray":(\[[^\]]+\])')):
             info.stream_types.append(self.bitrate_2_type[data["bitrate"]])
             info.streams[self.bitrate_2_type[data["bitrate"]]] = {'container': 'flv', 'video_profile': data["desc"], 'src': ["%s&_t=%s000"%(unescape(data["playUrl"]),int(time.time()))], 'size': float('inf')}
 

4de508d4777ace19c01d8378b4f4830e19af5d3a
无语 <33444499+wuyu8512@users.noreply.github.com>
2020-07-27 23:10:23 +0800

    bilibili.live: fixed 原画 (#521)
    更换b站直播间取流的方式，修复原画获取

--- ykdl/extractors/bilibili/live.py
+++ ykdl/extractors/bilibili/live.py
@@ -14,19 +14,19 @@ api_url = 'https://api.live.bilibili.com/room/v1/Room/playUrl?'
 api1_url = 'https://api.live.bilibili.com/room/v1/Room/room_init?id={}'
 api2_url = 'https://api.live.bilibili.com/room/v1/Room/get_info?room_id={}'
 api3_url = 'https://api.live.bilibili.com/live_user/v1/UserInfo/get_anchor_in_room?roomid={}'
+api4_url = 'https://api.live.bilibili.com/xlive/web-room/v1/playUrl/playUrl?'
 
 class BiliLive(VideoExtractor):
     name = u'Bilibili live (哔哩哔哩 直播)'
 
     profile_type = [
-        (u'原画', 'BD'),
+        (u'原画', 'OG'),
+        (u'蓝光', 'BD'),
         (u'超清', 'TD'),
         (u'高清', 'HD'),
         (u'流畅', 'SD')
     ]
-    supported_stream_profile = [prf for prf, _ in profile_type]
     profile_2_type = dict(profile_type)
-
     sorted_format = [fmt for _, fmt in profile_type]
 
     def prepare(self):
@@ -51,25 +51,25 @@ class BiliLive(VideoExtractor):
             info.artist = artist = api3_data['data']['info']['uname']
             info.title = '{} - {}'.format(title, artist)
 
-        def get_live_info(q=0):
+        def get_live_info(qn=1):
             params = {
-                'player': 1,
+                'https_url_req': 1,
                 'cid': self.vid,
-                'platform': 'html5',
-                'quality': q,
-                'otype': 'json'
+                'platform': 'web',
+                'qn': qn,
+                'ptype': '16'
             }
-            data = json.loads(get_content(api_url + urlencode(params)))
+            data = json.loads(get_content(api4_url + urlencode(params)))
 
             assert data['code'] == 0, data['msg']
 
             data = data['data']
             urls = [random.choice(data['durl'])['url']]
-            qlt = data['current_quality']
-            aqlts = [int(x) for x in data['accept_quality']]
+            qlt = data['current_qn']
+            aqlts = {x['qn']: x['desc'] for x in data['quality_description']}
             size = float('inf')
             ext = 'flv'
-            prf = self.supported_stream_profile[4 - qlt]
+            prf = aqlts[qlt]
             st = self.profile_2_type[prf]
             if urls and st not in info.streams:
                 info.stream_types.append(st)
@@ -80,8 +80,8 @@ class BiliLive(VideoExtractor):
                     'size': size
                 }
 
-            if q == 0:
-                aqlts.remove(qlt)
+            if qn == 1:
+                del aqlts[qlt]
                 for aqlt in aqlts:
                     get_live_info(aqlt)
 

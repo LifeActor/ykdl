f48b08a15d2d62cab051c5cb69a5a4d24c59216f
SeaHOH <Seahoh@Gmail.com>
2021-02-03 18:59:43 +0800

    iqiyi: support gamelive.iqiyi.com
    close #550

--- ykdl/extractors/iqiyi/__init__.py
+++ ykdl/extractors/iqiyi/__init__.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from ykdl.util.html import get_location
+
 import re
 
 def get_extractor(url):
-    if 'live.iqiyi' in url:
+    if 'gamelive' in url:
+        url = get_location(url)
+    if 'pps.' in url:
+        from ykdl.extractors import pps as s
+    elif 'live.iqiyi' in url:
         from . import live as s
     else:
         from . import video as s

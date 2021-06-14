56a9b9fbe4751166f1ef0e6a7a64b42a74ec8626
SeaHOH <Seahoh@Gmail.com>
2021-06-14 22:04:12 +0800

    huya: update

--- ykdl/extractors/huya/live.py
+++ ykdl/extractors/huya/live.py
@@ -3,16 +3,35 @@
 
 from ykdl.extractor import VideoExtractor
 from ykdl.videoinfo import VideoInfo
-from ykdl.compact import unescape
-from ykdl.util.html import get_content, add_header
-from ykdl.util.match import match1, matchall
+from ykdl.util.html import get_content
+from ykdl.util.match import match1
 
 import json
+import time
 import base64
 import random
+import hashlib
+
+from html import unescape
+from urllib.parse import unquote
+
+
+def md5(s):
+    return hashlib.md5(s.encode()).hexdigest()
 
 class HuyaLive(VideoExtractor):
-    name = u"Huya Live (虎牙直播)"
+    name = 'Huya Live (虎牙直播)'
+
+    def profile_2_id_rate(self, profile):
+        if profile[-1] == 'M':
+            return profile.replace('蓝光', 'BD'), int(profile[2:-1]) * 1000
+        else:
+            return {
+                '蓝光': ('BD', 3000),
+                '超清': ('TD', 2000),
+                '高清': ('HD', 0),
+                '流畅': ('SD', 0)
+            }[profile]
 
     def prepare(self):
         info = VideoInfo(self.name, True)
@@ -20,33 +39,51 @@ class HuyaLive(VideoExtractor):
         html  = get_content(self.url)
 
         json_stream = match1(html, '"stream": "([a-zA-Z0-9+=/]+)"')
-        assert json_stream, "live video is offline"
+        assert json_stream, 'live video is offline'
         data = json.loads(base64.b64decode(json_stream).decode())
+        self.logger.debug('data:\n%s', data)
         assert data['status'] == 200, data['msg']
 
         room_info = data['data'][0]['gameLiveInfo']
-        info.title = u'{}「{} - {}」'.format(
+        info.title = '{}「{} - {}」'.format(
             room_info['roomName'], room_info['nick'], room_info['introduction'])
         info.artist = room_info['nick']
 
         stream_info = random.choice(data['data'][0]['gameStreamInfoList'])
+        sUrl = stream_info['sFlvUrl']
         sStreamName = stream_info['sStreamName']
+        sUrlSuffix = stream_info['sFlvUrlSuffix']
+        sAntiCode = unquote(unescape(stream_info['sFlvAntiCode']))
+        sAntiCode = dict(p.split('=', 1) for p in sAntiCode.split('&') if p)
         
-        def link_urls():
-            for sType in ('flv', 'hls'):
-                sType = sType.title()
-                sUrl = stream_info['s{}Url'.format(sType)]
-                sUrlSuffix = stream_info['s{}UrlSuffix'.format(sType)]
-                sAntiCode = stream_info['s{}AntiCode'.format(sType)]
-                yield u'{}/{}.{}?{}'.format(sUrl, sStreamName, sUrlSuffix, sAntiCode)
+        uid = '0'
+        t = '100'
+        ct = str(int(time.time() * 1e7))
+        ctype = sAntiCode['ctype']
+        wsTime = sAntiCode['wsTime']
+        cs = md5('|'.join([ct, ctype, t]))
+        fm = base64.b64decode(sAntiCode['fm']).decode().split('_', 1)[0]
 
-        info.stream_types.append("current")
-        info.streams["current"] = {
-            'container': 'flv',
-            'video_profile': 'current',
-            'src': [unescape(url) for url in link_urls()],
-            'size' : float('inf')
-        }
+        def link_url(rate):
+            if rate:
+                streamname = '{}_{}'.format(sStreamName, rate)
+            else:
+                streamname = sStreamName
+            wss = md5('_'.join([fm, uid, streamname, cs, wsTime]))
+            return '{}/{}.{}?wsSecret={}&wsTime={}&uid={}&seqid={}&ctype={}&ver=1&t={}'.format(
+                    sUrl, streamname, sUrlSuffix, wss, wsTime, uid, ct, ctype, t)
+
+        for si in data['vMultiStreamInfo']:
+            video_profile = si['sDisplayName']
+            stream, _rate = self.profile_2_id_rate(video_profile)
+            rate = si['iBitRate'] or _rate
+            info.stream_types.append(stream)
+            info.streams[stream] = {
+                'container': 'flv',
+                'video_profile': video_profile,
+                'src': [link_url(rate)],
+                'size' : float('inf')
+            }
         return info
 
 site = HuyaLive()

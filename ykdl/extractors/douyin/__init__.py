890fe84a702faeb40ecfbde1450f91862df8f679
SeaHOH <Seahoh@Gmail.com>
2021-08-11 23:19:59 +0800

    douyin: update

--- ykdl/extractors/douyin/__init__.py
+++ ykdl/extractors/douyin/__init__.py
@@ -7,7 +7,7 @@ from ykdl.util.html import get_location
 def get_extractor(url):
     if '/v.' in url:
         url = get_location(url)
-    if '/live.' in url:
+    if '/live.' in url or 'amemv.com' in url:
         from . import live as s
     else:
         from . import video as s
--- ykdl/extractors/douyin/live.py
+++ ykdl/extractors/douyin/live.py
@@ -15,6 +15,7 @@ class TikTok(VideoExtractor):
 
     stream_ids = ['OG', 'BD', 'TD', 'HD', 'SD']
     quality_2_profile_id = {
+        'ORIGION': ['原画', 'OG'],
         'FULL_HD1': ['蓝光', 'BD'],
         'HD1': ['超清', 'TD'],
         'SD1': ['高清', 'HD'],
@@ -25,12 +26,16 @@ class TikTok(VideoExtractor):
         info = VideoInfo(self.name)
 
         html = get_content(self.url)
-        data = match1(html, 'id="RENDER_DATA" type="application/json">(.+?)</script>')
+        data = match1(html, 'id="RENDER_DATA" type="application/json">(.+?)</script>',
+                            '__INIT_PROPS__ = (.+?)</script>')
         data = json.loads(unquote(data))
         self.logger.debug('data: \n%s', data)
 
-        video_info = data['routeInitialProps']['roomInfo'].get('room')
-        assert video_info, 'live is off!!!'
+        try:
+            video_info = data['initialState']['roomStore']['roomInfo'].get('room')
+        except KeyError:
+            video_info = data['/webcast/reflow/:id'].get('room')
+        assert video_info and video_info['status'] == 2, 'live is off!!!'
 
         title = video_info['title']
         nickName = video_info['owner']['nickname']
@@ -39,8 +44,11 @@ class TikTok(VideoExtractor):
 
         stream_info = video_info['stream_url']
         stream_url = stream_info['flv_pull_url']
+        stream_url['ORIGION'] = stream_info.get('rtmp_pull_url')
         
         for ql, url in stream_url.items():
+            if not url:
+                continue
             video_profile, stream = self.quality_2_profile_id[ql]
             info.stream_types.append(stream)
             info.streams[stream] = {

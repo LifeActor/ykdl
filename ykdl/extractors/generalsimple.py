518dfc6cdc35986a50e04476353a9d5c0383b8c7
SeaHOH <Seahoh@Gmail.com>
2021-08-17 12:33:34 +0800

    generalsimple: bugfix and update

--- ykdl/extractors/generalsimple.py
+++ ykdl/extractors/generalsimple.py
@@ -6,6 +6,25 @@ from ykdl.extractor import VideoExtractor
 from ykdl.util.match import match1
 from ykdl.util.html import get_content, url_info
 
+import json
+from html import unescape
+from urllib.parse import unquote
+
+
+pattern = r'''(?x)
+["'](
+    (https?:)?\\?/\\?/[^"']+?\.
+    (?:
+        m3u8                        | # !H5 list/HLS
+        mp4|webm                    | # video/audio
+        f4v|flv|ts                  | # !H5 video
+        mov|qt|m4[pv]|og[mv]        | # video
+        ogg|3gp|mpe?g               | # video/audio
+        mp3|flac|wave?|oga|aac|weba   # audio
+    )
+    (\?[^"']+)?
+)["']
+'''
 
 class GeneralSimple(VideoExtractor):
     name = 'GeneralSimple (通用简单)'
@@ -21,24 +40,12 @@ class GeneralSimple(VideoExtractor):
         info.title = match1(html, '<meta property="og:title" content="([^"]+)',
                                   '<title>(.+?)</title>')
 
-        url = match1(html, '''(?x)
-            ["'](
-                (?:https?:)?//[^"']+?\.
-                    (?:
-                        m3u8                        | # !H5 list/HLS
-                        mp4|webm                    | # video/audio
-                        f4v|flv|ts                  | # !H5 video
-                        mov|qt|m4[pv]|og[mv]        | # video
-                        ogg|3gp|mpe?g               | # video/audio
-                        mp3|flac|wave?|oga|aac|weba   # audio
-                    )
-                    (?:\?[^"']+)?
-            )["']
-        ''')
-        if not url.startswith('http'):
-            url = self.url[:self.url.find(':') + 1] + url
+        url = match1(html, pattern) or match1(unquote(unescape(html)), pattern)
 
         if url:
+            url = json.loads('"{url}"'.format(**vars()))
+            if url[0] == '/':
+                url = self.url[:self.url.find('/')] + url
             info.stream_types.append('current')
             info.streams['current'] = {
                 'container': url_info(url)[1],

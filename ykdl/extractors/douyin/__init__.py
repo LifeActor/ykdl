2d8f63c25a21ecd771364accc2f5eedd33cdd6df
SeaHOH <Seahoh@Gmail.com>
2022-11-27 20:40:04 +0800

    douyin: update

--- ykdl/extractors/douyin/__init__.py
+++ ykdl/extractors/douyin/__init__.py
@@ -4,11 +4,7 @@ from .._common import *
 
 
 def get_extractor(url):
-    if '/v.' in url:
-        add_header('User-Agent',
-                   'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) '
-                   'AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 '
-                   'Mobile/15E148 Safari/604.1')
+    if '/v.' in url or 'iesdouyin.' in url:
         url = get_location(url)
     if '/live.' in url or 'amemv.com' in url:
         from . import live as s

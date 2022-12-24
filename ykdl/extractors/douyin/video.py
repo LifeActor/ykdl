22689b10290dded22f4f8f9e5e2b4c097007144d
SeaHOH <Seahoh@Gmail.com>
2022-12-24 14:55:08 +0800

    douyin.video: update

--- ykdl/extractors/douyin/video.py
+++ ykdl/extractors/douyin/video.py
@@ -12,11 +12,12 @@ class Douyin(Extractor):
     def prepare(self):
         info = MediaInfo(self.name)
 
-        data = get_response('https://www.douyin.com/web/api/v2/aweme/iteminfo/',
-                            params={'item_ids': self.mid}).json()
-        assert not data['filter_list'], data['filter_list'][0]['filter_reason']
+        data = get_response('https://www.iesdouyin.com/aweme/v1/web/aweme/detail/',
+                            params={'aweme_id': self.mid}).json()
+        assert data['status_code'] == 0, data['status_msg']
+        assert data['aweme_detail'], data['filter_detail']
 
-        video_info = data['item_list'][0]
+        video_info = data['aweme_detail']
         title = video_info['desc']
         nickName = video_info['author'].get('nickname', '')
         uid = video_info['author'].get('unique_id') or \
